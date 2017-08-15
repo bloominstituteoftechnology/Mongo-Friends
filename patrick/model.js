165c8edb2a357fea4b0d0c5d4c18f951622af183
mixelpixel <mixelpix@mac.com>
2017-08-15 00:33:24 -0400

    User db model

--- patrick/model.js
+++ patrick/model.js
@@ -1,1 +1,18 @@
-// stuff goes here
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const BearSchema = new mongoose.Schema({
+  username: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
