8c4eca58aabe2f119183ce35c77aa3bc4d293e91
John Wells <molekewl2017@gmail.com>
2017-08-15 15:39:50 -0700

    Extra Credit BlogPosts get/post/delete posts

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const UserSchema = require('./models.js');
+const PostSchema = require('./blogPosts.js');
 
 const STATUS_USER_ERROR = 422;
 const STATUS_SERVER_ERROR = 500;
@@ -60,9 +61,58 @@ server.delete('/users/:id', (req, res) => {
   });
 });
 
+// GET '/posts' return an array of blog posts
+server.get('/blogPosts', (req, res) => {
+  PostSchema.find({}, (err, posts) => {
+    if (err) throw err;
+    res.json(posts);
+  });
+});
+// GET '/posts/:id' return blog post with matching id
+server.get('/blogPosts', (req, res) => {
+  const { id } = req.params;
+  PostSchema.findById(id, (err, post) => {
+    if (err) throw err;
+    res.json(post);
+  });
+});
+// POST '/post' should save a new blog post to the server
+server.post('/blogPosts', (req, res) => {
+  const { title, post } = req.body;
+  if (!title || !post) {
+    res.status(STATUS_SERVER_ERROR);
+    res.json({ error: 'Missing title or content'});
+    return;
+  }
+  // create & save new posts
+  const singlePost = new PostSchema({ title, post });
+  singlePost.save((err) => {
+    if (err) throw err;
+    res.json(singlePost);
+  });
+});
+// DELETE '/posts/:id' delete a blog post
+server.delete('/blogPosts/:id', (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    res.status(STATUS_SERVER_ERROR);
+    res.json({ error: 'must provide a blog post id'});
+    return;
+  }
+  const post = PostSchema.findByIdAndRemove(id, (err, post) => {
+    if (!post) {
+      res.status(STATUS_SERVER_ERROR);
+      res.json({ error: `Couldn't find post with id ${id}` });
+      return;
+    }
+    res.json(post);
+  });
+});
+
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
-  'mongodb://localhost/users',
+  // 'mongodb://localhost/users',
+  'mongodb://localhost/blogPosts',
   { useMongoClient: true }
 );
 
