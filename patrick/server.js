e385fdd3b365ca5b0a369ddb7e03ea1c93743315
mixelpixel <mixelpix@mac.com>
2017-08-15 00:34:22 -0400

    Users db server

--- patrick/server.js
+++ patrick/server.js
@@ -1,1 +1,28 @@
-// stuff goes here
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models.js');
+
+const STATUS_USER_ERROR = 422;
+const STATUS_SERVER_ERROR = 500;
+const server = express();
+
+server.use(bodyParser.json());
+
+
+mongoose.Promise = global.Promise;
+const connect = mongoose.connect(
+  'mongodb://localhost/users',
+  { useMongoClient: true }
+);
+
+/* eslint no-console: 0 */
+connect.then(() => {
+  const port = 3000;
+  server.listen(port);
+  console.log(`Server Listening on ${port}`);
+}, (err) => {
+  console.log('\n************************');
+  console.log("ERROR: Couldn't connect to MongoDB. Do you have it running?");
+  console.log('************************\n');
+});
