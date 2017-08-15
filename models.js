fc3ddc254afd7f8a1aac4ccb5b51831bc9823a49
Latoyya Smith <latoyyas@gmail.com>
2017-08-15 14:13:16 -0500

    updated models.js and server.js

---
+++ models.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  // TODO: write your schema here
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
