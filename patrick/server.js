ac18aa1aa21bf14e255f10a543846bd812ee3bb5
mixelpixel <mixelpix@mac.com>
2017-08-15 22:01:15 -0400

    Extra Credit √

--- patrick/server.js
+++ patrick/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./models/userModel.js');
 const Blog = require('./models/blogModel.js');
+// const userRouter = require('./routes/userRoute.js');
+// const blogRouter = require('./routes/blogRoute.js');
 
 const STATUS_USER_ERROR = 422;
 const STATUS_SERVER_ERROR = 500;
@@ -14,7 +16,7 @@ server.post('/users', (req, res) => {
   const { name } = req.body;
   if (!name) {
     res.status(STATUS_USER_ERROR);
-    res.json({ error: 'Please entaer a NAME.' });
+    res.json({ error: 'Please enter a NAME.' });
     return;
   }
   const user = new User({ name });
@@ -50,57 +52,13 @@ server.get('/users/:id', (req, res) => {
   });
 });
 
-// // FIND BY ID AND REMOVE
-// server.delete('/users/:id', (req, res) => {
-//   const { id } = req.params;
-//   User.findByIdAndRemove(id, (err, user) => {
-//     if (err) {
-//       res.status(STATUS_SERVER_ERROR);
-//       res.json({ error: `There is no '${err.value}' to DELETE: ${err}` });
-//     } else if (user === null) {
-//       res.json({ error: `There's no user to DELETE at id: ${id} >>> ${user}.` });
-//     } else {
-//       res.json({ error: `'${user.name}' has been deleted.` });
-//     }
-//   });
-// });
-
-
-// // FIND & REMOVE Method
-// // per: http://mongoosejs.com/docs/api.html#query_Query-remove
-// server.delete('/users/:id', (req, res) => {
-//   const id = req.params.id;
-//   // const { id } = req.params;
-//
-//   const toDelete = User.find({ _id: id });
-//   // console.log(toDelete);
-//
-//   const query = User.find().remove({ _id: id });
-//
-//   query.remove({ _id: id }, (err, delUser) => {
-//     if (err) {
-//       res.status(STATUS_SERVER_ERROR);
-//       // res.json(err);
-//       res.json({ error: `There is no '${err.value}' to DELETE` });
-//     } else if (delUser.result.n === 0) {
-//       res.json({ error: 'There is nothing to delete' });
-//     } else {
-//       // delUser is whatever the callBack is returning
-//       res.json(delUser);
-//       // res.json({ error: `'${delUser.name}' has been deleted.` });
-//     }
-//   });
-// });
-
-
-// REMOVE Method
 server.delete('/users/:id', (req, res) => {
   const { id } = req.params;
+
   User.remove({ _id: id }, (err, delUser) => {
     if (err) {
       res.status(STATUS_SERVER_ERROR);
-      // res.json(err);
-      res.json({ error: `There is no '${err.value}' to DELETE` });
+      res.json(err);
     } else if (delUser.result.n === 0) {
       res.json({ error: 'There is nothing to delete' });
     } else {
@@ -108,6 +66,63 @@ server.delete('/users/:id', (req, res) => {
     }
   });
 });
+
+// BLOG BLOG BLOG BLOG BLOG BLOG BLOG BLOG BLOG BLOG BLOG BLOG BLOG BLOG BLOG
+server.post('/posts', (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'Please enter a TITLE and CONTENTS.' });
+    return;
+  }
+  const user = new Blog({ title, contents });
+  user.save((err) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(user);
+    }
+  });
+});
+
+server.get('/posts', (req, res) => {
+  Blog.find({}, (err, posts) => {
+    if (err) {
+      res.status(STATUS_USER_ERROR);
+      res.json(err);
+    } else {
+      res.json(posts);
+    }
+  });
+});
+
+server.get('/posts/:id', (req, res) => {
+  const { id } = req.params;
+  Blog.findById(id, (err, user) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR);
+      res.json({ error: `There is no record of the id: ${err.value}.` });
+    } else {
+      res.json(user);
+    }
+  });
+});
+
+server.delete('/posts/:id', (req, res) => {
+  const { id } = req.params;
+
+  Blog.remove({ _id: id }, (err, delBlog) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR);
+      res.json(err);
+    } else if (delBlog.result.n === 0) {
+      res.json({ error: 'There is nothing to delete' });
+    } else {
+      res.json(delBlog);
+    }
+  });
+});
+
 
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
