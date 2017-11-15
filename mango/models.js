1b8dc649a1e5fc12267725de977bfeef5787e075
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-11-15 16:24:08 -0700

    BlogPosts GET | GET:ID | POST | DELETE finished

--- mango/models.js
+++ mango/models.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, },
   createdAt: { type: Date, default: Date.now, },
+  loggedIn: { type: Boolean, default: false, },
 });
 
 module.exports = mongoose.model('User', UserSchema);
