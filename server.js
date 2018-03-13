e1136437ba85318ba0a2bf2995856871f130db10
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-13 16:56:12 -0400

    Commit 2 - Post, Put and Get
    - Created Get and put routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const Friend = require('./models/friendModel.js');
 
 const server = express();
@@ -21,35 +22,70 @@ server.get('/api/friends', (req, res) => { // get whole list
 });
 
 server.post('/api/friends', (req, res) => {
-const { firstName, lastName, createdOn, age
-} = req.body;
-if ( !firstName || !lastName || !age ) {
-    res.status(400).json({ error: "Please provide first name, last name, and age for the friend." });
-}
-if (age < 1 || age > 120) {
-    res.status(400).json({ error: "Age must be an integer between 1 and 120."})
-    if (age % 1 !== 0) {
-        res.status(400).json({ error: "Age must be a whole number!"})
-    }
-    
-}
-const friend = new Friend({ firstName, lastName, createdOn, age });
-friend.save()
-.then(savedFriend => {
-    res.status(201).json(savedFriend);
-})
-.catch(err => {
-    res.status(500).json({ errorMessage: "There was an error while saving the Bear to the Database" });
-})
+    const { firstName, lastName, createdOn, age } = req.body;
+    const friend = new Friend({ firstName, lastName, createdOn, age });
 
+    friend.save()
+        .then(savedFriend => {
+            res.status(201).json(savedFriend);
+        })
+        .catch(err => {
+            res.status(500).json({ msg: 'err'})
+        })
 });
 
-// server.get('/api/friends', (req, res) => {
-//     const { id } = req.params // get by ID
+// server.post('/api/friends', (req, res) => {
+//     const { firstName, lastName, createdOn, age } = req.body;
+// //     if ( !firstName || !lastName || !age ) {
+// //         res.status(400).json({ error: "Please provide first name, last name, and age for the friend." });
+// //     }
+// //     if (age < 1 || age > 120) {
+// //         res.status(400).json({ error: "Age must be an integer between 1 and 120."})
+// //             if (age % 1 !== 0) {
+// //             res.status(400).json({ error: "Age must be a whole number!"})
+// //         }
+// // }
+//     const friend = new Friend({ firstName, lastName, createdOn, age });
+// friend.save()
+// .then(savedFriend => {
+//     res.status(201).json(savedFriend);
+// })
+// .catch(err => {
+//     res.status(500).json({ errorMessage: "There was an error while saving the Bear to the Database" });
+// })
 // });
 
-// server.put('/api/friends') // to edit the friend
+server.get('/api/friends/:id', (req, res) => {
+    const { id } = req.params; // get by ID
 
+    Friend.findById(id)
+        .then(friend => {
+            if(friend) {
+                res.status(200).json(product);
+            } else {
+                res.status(404).json({ message: "The friend with the specified ID does not exist."})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The information could not be retrieved." })
+        })
+});
+
+server.put('/api/friends', (req, res) => {
+    const { id } = req.params;
+    const friendInfo = req.body;
+    Friend.findByIdAndUpdate(id, friendInfo)
+        .then(friend => {
+            if(friend) {
+                res.status(200).json(friend);
+            } else {
+                res.status(404).json({ message: "The friend with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error updating friend'})
+        })
+}) // to edit the friend
 
 
 
