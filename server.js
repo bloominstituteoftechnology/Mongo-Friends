e3ed34bac067455afc8ffd22942570a4caf4f320
Terrie Boeun Kim <terr1e@Terries-iMac.local>
2018-03-13 13:49:01 -0700

    POST and GET working!

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Friend = require('./friends/FriendModel');
+
+const server = express();
+
+server.use(helmet());
+server.use(bodyParser.json());
+
+server.get('/', function(req, res) {
+  res.status(200).json({ api: 'running...' });
+});
+
+server.post('/api/friends', (req, res) => {
+  const FriendInfo = req.body;
+  const {
+    firstName,
+    lastName,
+    age
+  } = req.body;
+
+  if (!firstName || !lastName || !age) {
+    console.log(error);
+    res.status(400).json({ errorMessage: "Please provide firstName, lastName and age for the friend." });
+  } else if (age < 1 || age > 120) {
+    console.log(error);
+    res.status(400).json({ errorMessage: "Age must be a whole number between 1 and 120" });
+  }
+  const friend = new Friend(FriendInfo);
+
+  friend
+    .save()
+    .then(newFriend => {
+      res.status(201).json(newFriend);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "There was an error while saving the friend to the database"  });
+    });
+});
+
+server.get('/api/friends', (req, res) => {
+  Friend.find({})
+    .then(friends => {
+      res.status(200).json(friends);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "The information could not be retrieved." });
+    });
+});
+
+
+
+mongoose
+  .connect('mongodb://localhost/friends')
+  .then(conn => {
+    console.log('connected to mongo');
+  })
+  .catch(err => {
+    console.log('error connecting to mongo');
+  });
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`running on port ${port}`));
