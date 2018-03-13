c2e68bad786181943d90b172fc8429b0f4a13a9d
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-03-13 16:15:28 -0500

    POST Route/Schema
    in FriendModel.js:
    define Schema properties
    in FriendRoutes.js:
    create POST function
    save new friend
    throw error if all data is not present
    throw error if saving fails

--- Friends/FriendModel.js
+++ Friends/FriendModel.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
 const FriendSchema = mongoose.Schema({
-  name: {
+  firstName: {
     type: String,
+    required: true,
   },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: [1, 'The age should be 1 or greater'],
+    max: [120, 'The age should be 120 or less'],
+  }
 });
 
 const FriendModel = mongoose.model('Friend', FriendSchema);
--- Friends/FriendRoutes.js
+++ Friends/FriendRoutes.js
@@ -2,8 +2,20 @@ const express = require('express');
 const friendRouter = express.Router();
 const Friend = require('./FriendModel');
 
-friendRouter.get('/', function(req, res) {
-  res.status(200).json({ message: `Your Router Ain't Fucked Up`});
+friendRouter.post('/', function(req, res) {
+  const friendInfo = req.body;
+  const { firstName, lastName, age } = friendInfo;
+  if (!firstName || !lastName || !age) {
+    res.status(400).json({ errorMessage: 'Please provide firstName, lastName and age for the friend.' });
+  }
+  const friend = new Friend(friendInfo);
+  friend.save()
+    .then(friend => {
+      res.status(201).json(friend);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: 'There was an error while saving the friend to the database', err});
+    });
 });
 
 module.exports = friendRouter;
