ac7a0dc75d2ee0ced816a3da9bc7da9c5d566cfc
Dixie Korley <dixiekorley@gmail.com>
2018-03-13 15:16:57 -0400

    Server.js complete

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ server.get('/', (req, res) => {
     res.status(200).json({ status: 'API Running' });
 });
 
+//router not defined yet 
+
+server.use('/api/friends', friendsRouter);
+mongoose
+    .connect(`mongodb://localhost/api`)
+    .then(conn => {
+        console.log(`Connected to Mongo`);
+    })
+    .catch(err => {
+        console.log(`Error: Connect to Mongo`);
+    })
 
 const port = process.env.PORT || 5005;
 server.listen(port, () => {
