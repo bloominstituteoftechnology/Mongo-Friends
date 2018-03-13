1e8b8b70a78407c5f70b699f4b2000695641ee17
Christopher Beards <beardscm@gmail.com>
2018-03-13 17:39:07 -0400

    Blog POST is functional

--- blog/BlogRoutes.js
+++ blog/BlogRoutes.js
@@ -3,8 +3,30 @@ const Blog= require('./BlogModel.js');
 
 const blogRouter = express.Router();
 
+
 //=========================
 //      blog POST
+//=========================
+
+blogRouter.post('/', (req, res) => {
+  const postInfo = req.body;
+  if (!postInfo.title || !postInfo.body) {
+    res.status(400).json({ errorMessage: "Please provide a Title and a Body for the post." });
+  } else {
+  const post = new Blog(postInfo);
+  post
+    .save()
+    .then(savedPost => {
+      res.status(201).json(savedPost);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error while saving the post to the database" });
+    });
+  }
+});
+
+//=========================
+//      blog GET
 //=========================
 
 blogRouter.get('/', (req, res) => {
