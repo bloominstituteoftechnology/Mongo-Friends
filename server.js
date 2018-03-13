ce5432a9733175269c75b6f1dbae78b15495a395
Amanda Phillips <amanda@phillipsdev.org>
2018-03-13 14:30:34 -0500

    server.js up and running

--- server.js
+++ server.js
@@ -2,3 +2,24 @@ const express = require("express");
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
+const server = express();
+
+server.use(helmet());
+server.use(bodyParser.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "running..." });
+});
+
+mongoose
+  .connect("mongodb://localhost/users")
+  .then(conn => {
+    console.log("connected to mongo");
+  })
+  .catch(err => {
+    console.log("error connecting to mongo");
+  });
+
+const port = process.env.PORT || 3030;
+server.listen(port, () => console.log(`running on port ${port}`));
