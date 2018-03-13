fce056f67d475ef44bff8bdfd44a448bbf0258d5
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-03-13 17:41:29 -0400

    Finished Routes, update functioning oddly

--- friends/friendRoutes.js
+++ friends/friendRoutes.js
@@ -54,11 +54,34 @@ friendsRouter.delete('/:id', (req, res) => {
           if (friend) {
               res.status(200).json(friend);
           } else {
-              res.status(404).json({ msg: "Not Found" })
+              res.status(404).json({ msg: "The friend with the specified ID does not exist." })
           }
       })
       .catch(err => {
           res.status(500).json({ msg: "Error deleting the friend", error: err })
+      });
+});
+
+friendsRouter.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const friendData = req.body;
+
+    Friend.findByIdAndUpdate(id, friendData, { new: true }) 
+      .then(friend => {
+          if (friend) {
+              res.status(200).json(friend);
+          } else {
+              res.status(404).json({msg: "The friend with the specified ID does not exist." })
+          }
+      })
+      .catch(err => {
+          if (err.name === "ValidatorError"){
+              res.status(400).json({ msg: "Age must be a whole number between 1 and 120", error: err })
+          } else if (err) {
+            res.status(400).json({ msg: "Please provide firstName, lastName and age for the friend.", error: err })
+          } else {
+              res.status(500).json({ msg: "The friend information could not be modified.", error: err})
+            }
       });
 });
 
