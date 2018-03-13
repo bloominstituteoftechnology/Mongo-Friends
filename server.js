b01eee0702dd7a0f24d286c23656335d4a34dbc1
Michael Marshal <mikemarshalman@gmail.com>
2018-03-13 16:03:02 -0500

    setting up mongoose and listener

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 // server
+const express = require('express');
+const mongoose = require('mongoose');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({status: 'API Running'});
+});
+
+mongoose
+  .connect('mongodb://localhost/FriendsList')
+  .then(db => {
+    console.log(`Connected to ${db.connections[0].name}`);
+  })
+  .catch(error => {
+    console.log('Failed to connect to the Database');
+  })
+
+const port = 5005;
+server.listen(port, () => {
+  console.log(`API running on http://localhost:${port}`);
+});
