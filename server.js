f4b71c79a2d49e75ac848360667506cf47393ec6
Christopher Beards <beardscm@gmail.com>
2018-03-13 15:02:16 -0400

    Connected to MongoDB

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const server = express();
-
+// const bearRouter = require('./bears/BearRoutes');
 
 server.use(helmet()); 
 server.use(cors());  
@@ -13,6 +13,16 @@ server.use(bodyParser.json());
 server.get('/', function(req, res) {
   res.status(200).json({ status: 'API Running' });
 });
+
+mongoose
+  .connect('mongodb://localhost/friends')
+  .then(conn => {
+    console.log('Successfully Connected to MongoDB');
+  })
+  .catch(err => {
+    console.log('Database connection failed');
+  });
+
 
 const port = process.env.PORT || 5005;
 server.listen(port, () => {
