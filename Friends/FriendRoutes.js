2c7ea2f25f2943d141004c9bcbb5d9a05a43231b
Jonathan Matthew Brunt <jonbrunt@Jonathans-MacBook-Pro.local>
2018-03-13 16:27:16 -0500

    Complete POST functionality
    in FriendRoutes.js:
    handle age error in catch
    handle saving error in catch

--- Friends/FriendRoutes.js
+++ Friends/FriendRoutes.js
@@ -14,6 +14,10 @@ friendRouter.post('/', function(req, res) {
       res.status(201).json(friend);
     })
     .catch(err => {
+      console.log(err._message);
+      if (err._message === 'Friend validation failed') {
+        res.status(400).json({ errorMessage: 'Age must be a whole number between 1 and 120'});
+      }
       res.status(500).json({ errorMessage: 'There was an error while saving the friend to the database', err});
     });
 });
