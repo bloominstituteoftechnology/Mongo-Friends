1074c5a1f50e44a2b7df70eb88fe32b82f191396
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-13 19:09:44 -0400

    implemented delete

--- friends/friendRoutes.js
+++ friends/friendRoutes.js
@@ -16,8 +16,8 @@ const routes = (Friend) => {
   friendRouter.post('/friends', (req, res) => {
     const friend = new Friend(req.body);
     friend.save()
-      .then(person => {
-        res.status(201).json(person);
+      .then(friend => {
+        res.status(201).json(friend);
       })
       .catch(err => {
         res.status(400).json({ errorMessage: "Please provide firstName, lastName and age for the friend." });
@@ -27,10 +27,14 @@ const routes = (Friend) => {
   friendRouter.get('/friends/:id', (req, res) => {
     Friend.findById(req.params.id)
       .then(friend => {
-        res.status(200).json(friend);
+        if (friend) {
+          res.status(200).json(friend);
+        } else {
+          res.status(404).json({ message: 'The friend with the specified ID does not exist.', error: err });
+        }
       })
       .catch(err => {
-        res.status(500).json({ error: 'The information could not be retrieved.'});
+        res.status(500).json({ error: 'The information could not be retrieved.', error: err });
       });
   });
 
@@ -42,11 +46,30 @@ const routes = (Friend) => {
         if (friend) {
           res.status(200).json(friend);
         } else {
-          res.status(404).json({ message: 'The friend with the specified ID does not exist.' });
+          res.status(404).json({ message: 'The friend with the specified ID does not exist.', error: err });
         }
       })
       .catch(err => {
-        res.status(500).json({ error: 'The friend information could not be modified.' });
+        res.status(500).json({ error: 'The friend information could not be modified.', error: err });
+      });
+  });
+
+  friendRouter.delete('/friends/:id', (req, res) => {
+    const { id } = req.params;
+    Friend.findByIdAndRemove(id)
+      .then(friend => {
+        if (friend) {
+          res.status(200).json(friend);
+        } else {
+          res.status(404).json({ message: 'The friend with the specified ID does not exist.', error: err });
+        }
+      })
+      .catch(err => {
+        if (err.name === 'CastError') {
+          res.status(400).json({ errorMessage: 'Please provide firstName, lastName and age for the friend.' });
+        } else {
+          res.status(500).json({ error: 'The friend information could not be modified.', error: err });
+        }
       });
   });
 
