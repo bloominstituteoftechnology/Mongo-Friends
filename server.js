878633165382088c53e026d8e97c0aeff6dbb425
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-13 16:39:30 -0400

    implemented HTTP get

--- server.js
+++ server.js
@@ -10,11 +10,20 @@ app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 
+const friendRouter = express.Router();
+
+friendRouter.get('/friends', (req, res) => {
+  const response = { hello: 'This is my API' };
+  res.json(response);
+});
+
+app.use('/api', friendRouter);
+
 app.get('/', (req, res) => {
   res.status(200).json({ status: 'API Running...' });
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, () {
+app.listen(port, () => {
   console.log(`API running on http://localhost:${port}`);
 });
