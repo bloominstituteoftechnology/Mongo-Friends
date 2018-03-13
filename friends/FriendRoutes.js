2096bb1c9d2721368c34f4adc17b5bc0cf1f47b8
Christopher Beards <beardscm@gmail.com>
2018-03-13 18:55:48 -0400

    Updated Error Message

--- friends/FriendRoutes.js
+++ friends/FriendRoutes.js
@@ -46,10 +46,14 @@ friendRouter.get('/:id', (req, res) => {
   const { id } = req.params;
   Friend.findById(id)
     .then(friend => {
+      if (!friend) {
+      res.status(404).json({ message: "The friend with the specified ID does not exist." });
+      } else {
       res.status(200).json(friend);
+      }
     })
     .catch(err => {
-      res.status(404).json({ message: "The friend with the specified ID does not exist." });
+      res.status(500).json({ error: "The information could not be retrieved." });
     });
 });
 
@@ -79,8 +83,9 @@ friendRouter.delete('/:id', (req, res) => {
 friendRouter.put('/:id', (req, res) => {
   const { id } = req.params;
   const friendInfo = req.body;
-  if (!friendInfo.firstName || !friendInfo.lastName || !friendInfo.age)
+  if (!friendInfo.firstName || !friendInfo.lastName || !friendInfo.age) {
     res.status(400).json({ errorMessage: 'Please provide firstName, lastName and age for the friend.' });
+  }
   else {
     Friend.findByIdAndUpdate(id, friendInfo, { new: true })
       .then(friend => {
