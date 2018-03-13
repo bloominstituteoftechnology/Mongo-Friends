e4430ba77da58c1ceea630108459bdf4eacec4e5
rawalhome@gmail.com <rawalhome@gmail.com>
2018-03-13 17:37:55 -0500

    Daniel Abbott + Punit Rawal: stretch..done...all..done...time for a nap

--- blogs/blogModel/blogModel.js
+++ blogs/blogModel/blogModel.js
@@ -6,16 +6,17 @@ const blogSchema = new mongoose.Schema({
   userName:{
     type: String,
     required: true,
-    validate: [validateString, 'Custom error'],
+    validate: [validateuserName, 'That aint a string Bub!'],
   },
-  password: {
+  passWord: {
     type: String,
     required: true,
-    validate: [validateString, 'Custom error'],
+    validate: [validatePwd, 'you want to be safe doncha? Put in a reasonable pwd'],
   },
   blogContent:{
     type: String,
     required: true,
+    validate: [validateBlog, 'TPut in some more words Bub!'],
   },
   createdOn:{
     type: Date,
@@ -23,8 +24,24 @@ const blogSchema = new mongoose.Schema({
   },
 });
 
-function validateString(string) {
-  
+function validateuserName(userName) {
+  if (typeof(userName) === 'string') {
+    return true;
+  }
+  return false;
+};
+
+function validateBlog(x) {
+  if (typeof(x) === 'string') {
+    return true;
+  }
+  return false;
+};
+
+//Pwd has to have a number..a capital letter..a special character
+function validatePwd(password) {
+    const regex1 = /(^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#\$%\^\&*\)\(+=._-]).{8,}$)/gm
+    return regex1.test(password); 
 };
 
 const BlogModel = mongoose.model('blog', blogSchema);
---
+++ blogs/blogRouter/blogRouter.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Blog = require('../blogModel/blogModel.js');
+
+const blogRouter = express.Router();
+
+blogRouter.post('/', function(req, res) {
+  const blogInfo = req.body;
+  const blog = new Blog(blogInfo);
+
+  blog.save()
+    .then(inBlog => {
+      res.status(201).json(inBlog);
+    })
+    .catch(err => {
+      if (err.errors.password) {
+        res.status(400).json({ errorMessage: err.errors.password.message }).end();
+      } else if (err.name === 'ValidationError') {
+        res.status(400).json({ errorMessage: err }).end();
+      } else {
+        res.status(500).json({ error: "There was an error while saving the blog to the database" }).end();
+      }
+    });
+});
+
+blogRouter.get('/', function(req, res) {
+  Blog.find({})
+    .then(blog => {
+      res.status(200).json(blog);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The information could not be retrieved." }).end();
+    });
+});
+
+blogRouter.get('/:id', function(req, res) {
+  const blogId = { _id: req.params.id };
+  Blog.findById(blogId)
+    .then(blog => {
+      res.status(200).json(blog)
+    })
+    .catch(err => {
+      if (err.name === 'CastError') {
+        res.status(404).json({ message: "The blog with the specified ID does not exist." });
+      } else {
+        res.status(500).json({ error: "The information could not be retrieved." }).end();
+      }
+    });
+});
+
+blogRouter.put('/:id', function(req, res) {
+  const blogId = { _id: req.params.id };
+  const updateBlog = req.body;
+  Blog.findByIdAndUpdate(blogId, updateBlog, { new: true } )
+    .then(doc => {
+      res.status(200).json(doc)
+    })
+    .catch(err => {
+      if (err.name === 'CastError') {
+        res.status(404).json({ message: "The blog with the specified ID does not exist." });
+      } else if (err.name === 'ValidationError') {
+        res.status(400).json({ errorMessage: "Please provide userName and pwd for the blog." }).end();
+      } else {
+        res.status(500).json({ error: "The blog information could not be modified." }).end();
+      }
+    });
+});
+
+blogRouter.delete('/:id', function(req, res) {
+  const blogId = { _id: req.params.id };
+  Blog.findByIdAndRemove(blogId)
+    .then(doc => {
+      res.status(200).json(doc)
+    })
+    .catch(err => {
+      if (err.name === 'CastError') {
+        res.status(404).json({ message: "The blog with the specified ID does not exist." });
+      } else {
+        res.status(500).json({ error: "The blog could not be removed" }).end();
+      }
+    });
+});
+
+module.exports = blogRouter;
