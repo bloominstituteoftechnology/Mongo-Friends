d9a06e65f76c491ad70938ffe7a9e70b1f0392b9
Charlie Sparks <csparks@antioch.edu>
2018-03-14 16:40:43 -0700

    initial setup working

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 /* eslint-disable */
+
+//module imports
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//initializing express
+const app = express();
+
+//setting middleware
+app.use(bodyParser.json());
+mongoose.Promise = global.Promise;
+
+//initialzing mongoose
+mongoose.connect("mongodb://localhost:27017/FriendsDB");
+mongoose.connection
+.once("open", () => console.log("The database is open"))
+.on("error", (err) => console.log(`There was an error starting the database: ${err}`));
+
+//starting the server
+app.listen(3000, () => console.log(`The Express server is listening at port 3000`));
