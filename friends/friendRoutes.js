721b8b16147754d0b691720efde433e519f65613
german gonzalez <gcomlnk@gmail.com>
2018-03-14 05:35:10 -0400

    finishing CRUD routing and connecting to mongodb

--- friends/friendRoutes.js
+++ friends/friendRoutes.js
@@ -13,4 +13,49 @@ friendRouter.get('/', (req, res) => {
         }));
 });
 
+friendRouter.get('/:id', (req, res) => {
+    Friend.findById(req.params.id)
+        .then(friend => {
+            if(friend) res.status(200).send(friend);
+            else res.status(404).send({ message: "The friend with the specified ID does not exist." });
+        })
+        .catch(err => {
+            res.status(500).send({ error: "The information could not be retrieved." });
+        });
+});
+
+friendRouter.post('/', (req, res) => {
+    const friend = new Friend(req.body);
+    friend
+        .save()
+        .then(friend => {
+            res.status(201).send(friend);
+        })
+        .catch(err => {
+            res.status(500).send({ error: "There was an error while saving the friend to the database", info: err});
+        });
+});
+
+friendRouter.delete('/:id', (req, res) => {
+    Friend.findByIdAndRemove(req.params.id)
+        .then(friend => {
+            if(friend) res.status(200).send(friend);
+            else res.status(404).send({ message: "The friend with the specified ID does not exist." });
+        })
+        .catch(err => {
+            res.status(500).send({ error: "The friend could not be removed" });
+        });
+});
+
+friendRouter.put('/:id', (req, res) => {
+    Friend.findByIdAndUpdate(req.params.id, req.body, { runValidators: true })
+        .then(friend => {
+            if(friend) res.status(200).send(friend);
+            else res.status(404).send({ message: "The friend with the specified ID does not exist." });
+        })
+        .catch(err => {
+            res.status(500).send({ error: "The friend information could not be modified.", info: err });
+        })
+});
+
 module.exports = friendRouter;
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const helmet = require('helmet');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-const server = express();
+const friendRouter = require('./friends/friendRoutes.js');
 
-server.use(helmet(), cors(), express.json());
+const server = express();
+server.use(helmet(), cors(), express.json(), bodyParser.json());
 
 server.get('/', (req, res) => {
     res.status(200).send({status: 'API Running'});
 });
 
+server.use('/api/friends', friendRouter);
+
+mongoose
+    .connect('mongodb://localhost/FriendKeeper')
+    .then(conn => console.log('connected to mongo'))
+    .catch(err => console.log('error connecting to mongo ' + err));
 
 const port = process.env.PORT || 5000;
 
-server.listen(port, () => console.log(`API running on http://localhost:${port}.`));
+server.listen(port, () => console.log(`API running on Port: ${port}.`));
