c820743d2f96c3bffd7818d77b86c1f34cfef97d
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-14 02:21:26 -0400

    Improved error handling for blogPosts POST request

--- blog-posts/blogPostsRouter.js
+++ blog-posts/blogPostsRouter.js
@@ -16,15 +16,23 @@ blogPostsRouter.post('/', (req, res) => {
   const newPost = req.body;
   const blogPost = new BlogPost(newPost);
 
-  blogPost.save().then(savedPost => {
-    res.status(201).json(savedPost);
-  })
-    .catch(err => {
-      res.status(500).json({
-        msg: 'There was an error while saving the friend to the databse',
-        err: err
-      })
-    })
+  if (!newPost.userName || !newPost.postContent) {
+    res.status(400).json({ err: 'You need to provide both a userName and postContent' })
+  }
 
-})
+  blogPost
+    .save()
+    .then(savedPost => {
+      res.status(201).json(savedPost);
+    })
+    .catch(err => {
+      if (err.errors.postContent.kind === 'maxlength') {
+        res.status(404).json({ err: 'Your post exceeded the max length' });
+      }
+      res.status(500).json({
+        msg: 'There was an error while saving the friend to the database',
+        err: err,
+      });
+    });
+});
 module.exports = blogPostsRouter;
