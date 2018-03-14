202c6a0827a8f12757feeff49cb48b187a76cc12
german gonzalez <gcomlnk@gmail.com>
2018-03-14 04:51:35 -0400

    adding validation

--- friends/friendModel.js
+++ friends/friendModel.js
@@ -4,6 +4,20 @@ const FriendSchema = new mongoose.Schema({
     firstName: {
         type: String,
         required: [true, 'Please provide firstName, lastName and age for the friend.']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Please provide firstName, lastName and age for the friend.']
+    },
+    age: {
+        type: Number,
+        min: [1, 'Age must be a whole number between 1 and 120.'],
+        max: [120, 'Age must be a whole number between 1 and 120.'],
+    },
+    createdOn: {
+        type: String,
+        required: true,
+        default: new Date(),
     }
 });
 
