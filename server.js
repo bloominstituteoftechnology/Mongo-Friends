37ce61220bdf1c6f3b291584a81fdf49a9354282
Carlos Andrade <carlos.andrade@outlook.com>
2018-04-09 16:23:19 -0400

    rob keeps getting unknown errors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ mongoose
   .then(() => console.log('\n <>--<>--<> connected to deez Mongo nutz <>--<>--<>\n'))
   .catch(err => console.log('You let your family down by not connecting to Mongo'))
 
-const friendController = require('./friends/friendModel');
+const friendController = require('./friends/friendController');
 
 const server = express();
 
@@ -19,6 +19,8 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
+server.use('/api/friends', friendController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
