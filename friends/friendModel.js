8824449726e873712c23d2b3cf670f57c122b5d6
Khepf <jeremy1975@gmail.com>
2018-04-09 18:08:43 -0400

    GET and POST requirements complete

---
+++ friends/friendModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const friendSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  }
+});
+
+const friendModel = mongoose.model('Friend', friendSchema);
+
+module.exports = friendModel;
