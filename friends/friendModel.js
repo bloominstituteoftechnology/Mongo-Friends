0baeb883a01564edfd9db7956c0f4919af9f0b50
mjd02 <mostdarryl@gmail.com>
2018-04-09 16:43:02 -0400

    server and database running successfully and get and post implemented

--- friends/friendModel.js
+++ friends/friendModel.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose');
-const friendSchema = new mongoose.Schema(){
+const friendSchema = new mongoose.Schema({
   firstName:{
     type:String,
     required:true,
-  }),
+  },
   lastName:{
     type:String,
     required:true,
-  }),
+  },
   age:{
     type:Number,
     required:true,
     minimum:1,
     maximum:120,
   },
-  createdOn{
+  createdOn:{
     type:Date,
     default:Date.now,
   },
 });
+const friendModel = mongoose.model('Friend',friendSchema);
+module.exports = friendModel;
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const friendController = require('./friends/friendController');
+const mongoose = require('mongoose');
 
 const server = express();
+
+mongoose.connect('mongodb://localhost/friendb')
+.then(()=>console.log('connected'))
+.catch(err=>{console.log('not connected')}
+);
 
 server.use(helmet());
 server.use(cors());
@@ -11,6 +18,8 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
+server.use('/api/friends',friendController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
