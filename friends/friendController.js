e5092b8c2c7a387a74e6e7a0550aae46e54a8da3
charletta <charlettabullard12@outlook.com>
2018-04-09 17:08:23 -0400

    Added GET and POST minus GET ID/DELETE and PUT request.WIP

--- friends/friendController.js
+++ friends/friendController.js
@@ -3,10 +3,24 @@ const router = require('express').Router();
 router
   .route('/')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/friends/' });
+    Friend.find({})
+      .then(friends => {
+        res.status(200).json(friends);
+      })
+      .catch(err => {
+        res.status(500).json({errorMessage: 'The friends information could not be retrieved.'})
+      });
   })
   .post((req, res) => {
-    res.status(201).json({ status: 'please implement POST functionality' });
+    const friend = new Friend(req.body);
+    friend
+      .save()
+      .then(savedFriend => {
+        res.status(201).json(savedFriend);
+      })
+      .catch(error => {
+        res.status(500).json({errorMessage: 'There was an error while saving the friend to the database.'})
+      })
   });
 
   router
