55292fb1a007c7c2a396a4bae08b02974713e427
charletta <charlettabullard12@outlook.com>
2018-04-09 15:45:47 -0400

    Installed mongoose and connect successfully to the mongo db

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/friends')
+  .then(() => console.log('\n=== connected to mongo ===\n'))
+  .catch(err => console.log('error connecting to mongo'));
 
 const friendController = require('./friends/friendController');
 
@@ -13,6 +19,8 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
+server.use('/api/friends', friendController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
