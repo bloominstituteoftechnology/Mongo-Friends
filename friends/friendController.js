b694cbf7c71551e1786443d62a6472f7022c048b
Drew Moody <drewmoody@Drews-MBP.fios-router.home>
2018-04-09 18:19:59 -0400

    basic functionality is working. Need to go back and refine error messaging/return messaging

---
+++ friends/friendController.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+
+const Friend = require('./friendModel');
+
+router
+  .route('/')
+  .get((req, res) => {
+    Friend.find({})
+    .then(friends => {
+      res.status(200).json(friends);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+  })
+  .post((req, res) => {
+    const friend = new Friend(req.body);
+
+    friend
+      .save()
+      .then(savedFriend => {
+        res.status(201).json(savedFriend);
+      })
+      .catch(err => res.status(500).json(err));
+  });
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    Friend
+    .findById(req.params.id)
+    .then(friends => {
+      res.status(200).json(friends);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+  })
+  .delete((req, res) => {
+      Friend
+      .findByIdAndRemove(req.params.id)
+      .then(response => {
+        res.status(200).json(response);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      })
+  })
+  .put((req, res) => {
+    Friend
+      .findByIdAndUpdate(req.params.id, req.body)
+      .then(response => {
+        res.status(200).json(response);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      })
+  });
+
+module.exports = router;
