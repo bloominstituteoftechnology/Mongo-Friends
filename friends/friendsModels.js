99ddaf412d8bfbd342b3b824c320086d6ccbedd6
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-04-09 17:17:39 -0400

    Got bug figured out - forgot mongoose stuff in friendsModels.js

--- friends/friendsModels.js
+++ friends/friendsModels.js
@@ -1,23 +1,25 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const friendSchema = new mongoose.Schema({
-//   firstName: {
-//     type: String,
-//     required: true
-//   },
-//   lastName: {
-//     type: String,
-//     required: true
-//   },
-//   age: {
-//     type: Number,
-//     min: 1,
-//     max: 120,
-//     required: true
-//   },
-//   createdOn: {
-//     date: Date
-//   }
-// });
+const friendSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    min: 1,
+    max: 120,
+    required: true
+  },
+  createdOn: {
+    date: Date
+  }
+});
 
-// module.exports = friendsModels;
+const friendsModels = mongoose.model('Friend', friendSchema);
+
+module.exports = friendsModels;
