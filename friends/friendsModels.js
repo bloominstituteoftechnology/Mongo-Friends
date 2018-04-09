2af6688d60c88b1cae75aa500f3d48bbd444cf31
Liz McKenzie <elizabethbraggisgreat@gmail.com>
2018-04-09 17:07:29 -0400

    Commented out bugged area

--- friends/friendsModels.js
+++ friends/friendsModels.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const friendSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  age: {
-    type: Number,
-    min: 1,
-    max: 120,
-    required: true
-  },
-  createdOn: {
-    date: Date
-  }
-});
+// const friendSchema = new mongoose.Schema({
+//   firstName: {
+//     type: String,
+//     required: true
+//   },
+//   lastName: {
+//     type: String,
+//     required: true
+//   },
+//   age: {
+//     type: Number,
+//     min: 1,
+//     max: 120,
+//     required: true
+//   },
+//   createdOn: {
+//     date: Date
+//   }
+// });
 
-module.exports = friendsModels;
+// module.exports = friendsModels;
