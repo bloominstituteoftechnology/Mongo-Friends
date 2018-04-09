0c0585afca3f90d78a2d35746aae47d07d2f549b
habib1234731 <habibrehman1234731@gmail.com>
2018-04-09 17:42:53 -0400

    Completed

---
+++ friends/friendsController.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const Friend = require('./friendsModel');
+
+router
+  .route('/')
+  .get((req, res) => {
+    Friend.find({})
+    .then(friends => {
+      res.status(200).json(friends);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+  })
+  .post((req, res) => {
+    const friend = new Friend(req.body);
+    friend.save()
+    .then(saveFriend => {
+      res.status(200).json(saveFriend);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+  });
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    Friend.findById(req.params.id)
+    .then(friends => {
+      res.status(200).json(friends);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+  })
+  .delete((req, res) => {
+    Friend.findByIdAndRemove(req.params.id)
+    .then(deleteFriend => {
+      res.status(200).json(deleteFriend);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+  })
+  .put((req, res) => {
+    Friend.findByIdAndUpdate(req.params.id, { $set: {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      age: req.body.age,
+  }})
+    .then(updateFriend => {
+      res.status(200).json(updateFriend);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+  });
+
+module.exports = router;
