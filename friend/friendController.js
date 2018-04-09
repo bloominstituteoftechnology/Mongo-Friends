ecd76d46f881d1aca2f2285bde19ee42a15af2f2
Kenny <khpham24@gmail.com>
2018-04-09 14:57:40 -0600

    errors complete
    need to do put and delete for tomorrow

--- friend/friendController.js
+++ friend/friendController.js
@@ -12,28 +12,49 @@ router
         res.status(500).json(err);
       });
   })
+
   .post((req, res) => {
     const friend = new Friend(req.body);
+    const { firstName, lastName, age } = req.body;
 
-    friend
-      .save()
-      .then(savedFriend => {
-        res.status(201).json(savedFriend);
-      })
-      .catch(err => res.status(500).json(err));
+    if (!firstName || !lastName || !age) {
+      res.status(400).json({
+        errorMessage:
+          'Please provide firstName, lastName and age for the friend.'
+      });
+    } else if (!age === Number || age < 1 || age > 120) {
+      res.status(400).json({
+        errorMessage: 'Age must be a number between 1 and 120'
+      });
+    } else {
+      friend
+        .save()
+        .then(savedFriend => {
+          res.status(201).json(savedFriend);
+        })
+        .catch(err => res.status(500).json(err));
+    }
   });
 
 router
   .route('/:id')
   .get((req, res) => {
-    Friend.findById(req.params.id)
-      .then(friends => {
-        res.status(200).json(friends);
-      })
-      .catch(err => {
-        res.status(500).json(err);
+    const id = req.body.id;
+    if (!id) {
+      res.status(404).json({
+        errorMessage: 'The friend with the specified ID does not exist.'
       });
+    } else {
+      Friend.findById(req.params.id)
+        .then(friends => {
+          res.status(200).json(friends);
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        });
+    }
   })
+
   .delete((req, res) => {
     res.status(200).json({ status: 'please implement DELETE functionality' });
   })
