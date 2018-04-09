e17799af2f8becc7703b3aa029664952039876d4
runranron <williamrpelton@gmail.com>
2018-04-09 15:27:45 -0500

    William Pelton WIP

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/friendDb')
+  .then(() => console.log('\n=== connected to mongo ===\n'))
+  .catch(err => console.log('error connecting to mongo'));
+
+const friendController = require('./friends/friendController');
 
 const server = express();
 
@@ -11,6 +19,8 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
+server.use('/api/friends', friendController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
