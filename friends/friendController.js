59b65580c11ad44828cf2754d9ac3ec1e7533873
William <wtpwinberg@gmail.com>
2018-04-09 16:05:21 -0700

    Get, getId, and post functioning

---
+++ friends/friendController.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+
+const Friend = require('./friendModel');
+
+// /api/friends
+router
+  .route('/')
+  .get((req, res) => {
+    Friend.find({})
+      .then(friends => {
+        res.status(200).json(friends);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  })
+  .post((req, res) => {
+    const friend = new Friend(req.body);
+
+    friend
+      .save()
+      .then(savedFriend => {
+        res.status(201).json(savedFriend);
+      })
+      .catch(err => res.status(500).json(err));
+  });
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    Friend.findById(req.params.id)
+      .then(friends => {
+        res.status(200).json(friends);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  })
+  .delete((req, res) => {
+    Friend.remove()
+      .then(friends => {
+        res.status(200).json(Friend);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  })
+  .put((req, res) => {
+    res.status(200).json({ status: 'please implement PUT functionality' });
+  });
+
+module.exports = router;
---
+++ friends/friendModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const friendSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const friendModel = mongoose.model('Friend', friendSchema);
+
+module.exports = friendModel;
