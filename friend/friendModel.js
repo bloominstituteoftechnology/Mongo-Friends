7b3340cae1387721846e9ac8be65cc584d4897ae
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-04-10 15:50:06 -0700

    put, delete added :)

---
+++ friend/friendModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const friendSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+  },
+  age: {
+    type: Number,
+    require: true,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const friendModel = mongoose.model('Friend', friendSchema);
+module.exports = friendModel;
