6700c5c408be26f6337b115b77de6d7fb74f5028
mjd02 <mostdarryl@gmail.com>
2018-04-10 18:23:05 -0400

    post functionality working

--- mongo-front/src/App.js
+++ mongo-front/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Get from './Components/Get/Get.js';
+import Post from './Components/Post/Post.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return(
+    <div>
       <Get/>
+      <Post/>
+    </div>
     );
   }
 }
