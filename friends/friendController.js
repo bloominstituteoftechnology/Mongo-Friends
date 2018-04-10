7dfcab385d5d6bb3b992598105c5aa28f3bb8db5
KJ Sourcing <tmkogwork@gmail.com>
2018-04-09 18:28:21 -0700

    crud complete

--- friends/friendController.js
+++ friends/friendController.js
@@ -3,42 +3,60 @@ const router = require('express').Router();
 const Friend = require('./friendModel.js');
 
 router
-  .route('/')
+  .route("/")
   .get((req, res) => {
     Friend.find({})
       .then(friends => {
         res.status(200).json(friends);
       })
-      .catch(error => {
-        res.status(500).json(error);
+      .catch(err => {
+        console.log("There was an error getting friends.");
       });
   })
   .post((req, res) => {
     const friend = new Friend(req.body);
+
     friend
       .save()
-      .then(savedFriend => res.json(savedFriend))
-      .catch(error => {
-        res.status(500).json(error);
+      .then(savedFriend => {
+        res.status(201).json(savedFriend);
+      })
+      .catch(err => {
+        console.log("Friend Saved!");
       });
   });
 
 router
-  .route('/:id')
+  .route("/:id")
   .get((req, res) => {
     Friend.findById(req.params.id)
-      .then(friends => {
-        res.status(200).json(friends);
+      .then(friend => {
+        res.json(friend);
       })
-      .catch(error => {
-        res.status(500).json(error);
+      .catch(err => {
+        console.log("There was a problem retrieving friend.");
       });
   })
   .delete((req, res) => {
-    res.status(200).json({ status: 'please implement DELETE functionality' });
+    Friend.findByIdAndRemove(req.params.id)
+      .then(() => {
+        res.json({ message: "Friend successfully deleted!" });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: "There was an error deleting Friend." });
+      });
   })
   .put((req, res) => {
-    res.status(200).json({ status: 'please implement PUT functionality' });
+    Friend.findByIdAndUpdate(req.params.id, req.body)
+      .then(friend => {
+        console.log("Successfully Updated Friend.");
+        res.status(201).json(friend);
+      })
+      .catch(err => {
+        console.log("There was a problem updating friend.");
+      });
   });
 
 module.exports = router;
