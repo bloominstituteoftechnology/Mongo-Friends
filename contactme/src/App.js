b00c757023708c82b85679ae1e742f764792aaac
astro11 <aquila.strother@asu.edu>
2018-04-10 17:12:49 -0400

    added styling, working on db post/data persistence

--- contactme/src/App.js
+++ contactme/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Friends from './Friends';
+import { Button, Input } from 'reactstrap';
 import axios from 'axios';
 
 class App extends Component {
@@ -9,8 +10,30 @@ class App extends Component {
     this.state = {
       friends: []
     };
+    this.handleAddFriend = this.handleAddFriend.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-  
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    console.log(this.state);
+  }
+
+  handleAddFriend(e) {
+    e.preventDefault();   
+    this.state.friends.push((this.state));
+    e.target.reset();    
+    this.setState({
+      firstName: '',
+      lastName: '',
+      age: 0,
+      email: ''
+      }        
+    )
+  }
+
   componentDidMount() {
     axios
     .get('http://localhost:5000/api/friends')
@@ -18,17 +41,26 @@ class App extends Component {
       this.setState({ friends: res.data });
     })
     .catch(error => {
-      console.log('There was an error retrieving friends');
+      console.log('There was an error retrieving friends, refresh once connected to MONGO');
     });  
   }
 
   render() {
     return (
       <div className="App">
-        <h2>Friends List</h2>
+        <div><strong>Contact</strong>Me</div>
+        <div>
+          <form className="form" onSubmit={this.handleAddFriend}>
+            <Input type="text" name="firstName" onChange={this.handleChange} placeholder="First Name" />
+            <Input type="text" name="lastName" onChange={this.handleChange} placeholder="Last Name" />
+            <Input type="text" name="age" onChange={this.handleChange} placeholder="Age" />
+            <Input type="text" name="email" onChange={this.handleChange} placeholder="Email Address" />
+            <Button type="submit">Add Friend</Button>
+          </form>
+          </div>
         {this.state.friends.map((friends, id) => {
           return (
-            <div key={id}>
+            <div className="contact" key={id}>
             <Friends contacts={friends} />
             </div>
           )
--- contactme/src/Friends.js
+++ contactme/src/Friends.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import './Friends.css';
 
 const Friends = (props) => {
     const friend = props.contacts;
-    return (
-        <div className="Table">
+    return (        
+        <div className="contacts">        
             <table>
                 <tbody>
                 <tr>
                     <th>First Name</th>
                     <th>Last Name</th>
                     <th>Age</th>
+                    <th>email</th>
                 </tr>
                 <tr>
                 <td> {friend.firstName} </td>
                 <td> {friend.lastName} </td>
                 <td> {friend.age} </td>
+                <td> {friend.email} </td>
                 </tr>
                 </tbody>
-            </table>
+            </table>            
         </div>
     )
 }
