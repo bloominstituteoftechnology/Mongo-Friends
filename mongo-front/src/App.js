d40b1b4ea60892ee280a26b1cec0cbec6b9751e2
mjd02 <mostdarryl@gmail.com>
2018-04-10 17:34:13 -0400

    working on front-end

---
+++ mongo-front/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Get from './Components/Get/Get.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return(
+      <Get/>
+    );
+  }
+}
+
+export default App;
