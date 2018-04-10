d4714d26862dfca77d09f0dfee931f24c1607972
Cristina Laryea <crispad06@gmail.com>
2018-04-10 16:52:56 -0400

    added more error mgs and if statements needed

--- friends/friendController.js
+++ friends/friendController.js
@@ -10,18 +10,32 @@ router
         res.status(200).json(friends);
       })
       .catch(err => {
-        res.status(500).json(err);
+        res.status(500).json({
+          errorMessage: "The friends information could not be retrieved."
+        });
       });
   })
   .post((req, res) => {
+    const { firstName, lastName, age } = req.body;
     const friend = new Friend(req.body);
 
-    friend
-      .save()
-      .then(savedFriend => {
-        res.status(201).json(savedFriend);
-      })
-      .catch(err => res.status(500).json(err));
+    if (!firstName || !lastName || !age) {
+      res.status(400).json({
+        errorMessage:
+          "Please provide firstName, lastName and age for the friend."
+      });
+    } else {
+      friend
+        .save()
+        .then(savedFriend => {
+          res.status(201).json(savedFriend);
+        })
+        .catch(err => {
+          res.status(500).json({
+            err: "There was an error while saving the friend to the database."
+          });
+        });
+    }
   });
 
 router
@@ -32,33 +46,51 @@ router
         res.status(200).json(friends);
       })
       .catch(err => {
-        res.status(500).json(err);
+        if (res.status(404)) {
+          res.json({
+            message: "The friend with the specified ID does not exist."
+          });
+        } else {
+          res.status(500).json({
+            errorMessage: "The friend information could not be retrieved."
+          });
+        }
       });
   })
   .delete((req, res) => {
     Friend.findByIdAndRemove(req.params.id)
       .then(response => {
         if (response === null) {
-          res.status(404).json({ message: "not found" });
+          res.status(404).json({
+            message: "The friend with the specified ID does not exist."
+          });
         } else {
           res.status(200).json(response);
         }
       })
       .catch(error => {
-        res.status(500).json(err);
+        res
+          .status(500)
+          .json({ errorMessage: "The friend could not be removed." });
       });
   })
   .put((req, res) => {
     Friend.findByIdAndUpdate(req.params.id, req.body)
       .then(response => {
         if (response === null) {
-          res.status(404).json({ message: "not found" });
+          res.status(404).json({
+            message: "The friend with the specified ID does not exist."
+          });
         } else {
           res.status(200).json(response);
         }
       })
       .catch(error => {
-        res.status(500).json(err);
+        res
+          .status(500)
+          .json({
+            errorMessage: "The friend information could not be modified."
+          });
       });
   });
 
