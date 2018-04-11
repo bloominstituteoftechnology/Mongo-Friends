4b83aa69c06ee6533da8190c4b6e81fc9f21db16
Shaun Kelly <Shaun@redskelly.local>
2018-04-11 02:27:03 -0400

    schema completed

--- friends/friendModel.js
+++ friends/friendModel.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-const friendSchema = new mongoose.Schema({});
+const friendSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  age: { type: Number, required: true, min: 1, max: 120 },
+  createdOn: { type: Date, required: true, default: Date.now },
+});
 
 const friendModel = mongoose.model('Friend', friendSchema); // this is the friends collection
 
