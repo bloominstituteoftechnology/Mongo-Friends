5dc17a0967cd0ba5fa8fd662eba0bf00d90074b2
totsuya <supertatortots@gmail.com>
2018-05-07 17:01:15 -0400

    added proper schema for friend

--- friends/friendsModel.js
+++ friends/friendsModel.js
@@ -1,1 +1,23 @@
 const mongoose = require('mongoose');
+
+const friendSchema = {
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+
+  age: {
+    type: Number,
+    max: 120,
+    min: 1
+  },
+  
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  }
+}
