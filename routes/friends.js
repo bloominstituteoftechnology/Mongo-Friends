92b66a6187add25290d2875e9ecad17f9e0465f0
Nedu Robert <nedumrobert@gmail.com>
2018-05-07 21:39:48 +0100

    added validation

--- routes/friends.js
+++ routes/friends.js
@@ -10,21 +10,30 @@ router
 function get(req, res) {
     Friends.find().then(friends => {
         res.status(200).json(friends);
-    });
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: 'The friends information could not be retrieved '});
+    })
 }
 
 function post(req, res) {
-    const newFriend = req.body;
+    const { firstName, lastName, age } = req.body;
+    const newFriend = { firstName, lastName, age };
     const friend = new Friends(newFriend);
-
+    if(!firstName || !lastName || !age) {
+        res.status(400).json({ errorMessage: 'Please provide firstName, lastName and age for the friend.' });
+    } else if(Number.isNaN(age) || (age < 1 || age > 120) ) {
+        res.status(400).json({ errorMessage: 'Age must be a number between 1 and 120' });
+    } else {
     friend
         .save()
         .then(friend => {
             res.status(201).json(friend);
         })
         .catch(err => {
-            res.status(500).json(err);
+            res.status(500).json({ errorMessage: 'There was an error while saving the friend to the database '});
         });
+    }
 }
 
 module.exports = router;
