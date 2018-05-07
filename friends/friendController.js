b3032f58a2c549c3cbb20a0db106b664582ad161
Paul Fang <paulfang@pauls-mbp.home>
2018-05-07 16:13:45 -0700

    working on friendController.js

---
+++ friends/friendController.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+
+const Friend = require('./friendModel');
+
+router
+    .route('/')
+    .get(get)
+    .post(post);
+
+router
+    .route('/:id')
+    .get (getbyId)
+    .put (put)
+    .delete (destroy)
+
+function get(req, res) {
+    Friend.find().then(friends => {
+        res.status(200).json(friends);
+    })
+}
+
+function post(req, res) {
+    const friendData = req.body;
+
+    const friend = new Friend(friendData);
+}
+
+    friend 
+        .save()
+        .then(friend => {
+            res.status(201).json(bear);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+
+    module.exports = router;
