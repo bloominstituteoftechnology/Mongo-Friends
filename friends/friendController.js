5583691b0a82bf2a4f0467f01d25a387c1157384
Devon Bacon <devon.bacon@gmail.com>
2018-05-07 13:03:35 -0700

    friendsController.js - set up default requests for id route

--- friends/friendController.js
+++ friends/friendController.js
@@ -23,5 +23,17 @@ router.route('/')
       });
   });
 
+router.route('/:id')
+  .get((req, res) => {
+    res.status(200).json({
+      route: '/api/friends/' + req.params.id
+    });
+  })
+  .delete((req, res) => {
+    res.status(200).json({ status: 'please implement DELETE functionality' });
+  })
+  .put((req, res) => {
+    res.status(200).json({ status: 'please implement PUT functionality' });
+  });
 
 module.exports = router;
