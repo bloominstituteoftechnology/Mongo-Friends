7de2d993778aa22bac3ba6f5e08a5f8910b07bf9
Yasin Shuman <email@yasin.io>
2018-05-07 16:22:11 -0700

    added logic to check age range and to check if its a number. it is working properly but checking if its a number is returning the wrong error code

--- Friends/friendController.js
+++ Friends/friendController.js
@@ -19,14 +19,20 @@ function post(req, res) {
   const friend = new Friend(friendData);
   const { firstName, lastName, age } = friend;
   if (firstName && lastName && age) {
-    friend
-      .save()
-      .then(friend => {
-        res.status(201).json(friend);
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
+    if (!isNaN(age) && age > 1 && age < 120) {
+      friend
+        .save()
+        .then(friend => {
+          res.status(201).json(friend);
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        });
+    } else {
+      res
+        .status(400)
+        .json({ errorMessage: "Age must be a number between 1 and 120" });
+    }
   } else {
     res.status(400).json({
       errorMessage: "Please provide firstName, lastName and age for the friend."
