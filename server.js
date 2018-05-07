0f5fd6905983e4adca47e06eb7bda3207a611446
unknown <bsa6.23.94@gmail.com>
2018-05-07 16:32:45 -0400

    finished all but stretch

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+ 
+
+const mongoose = require('mongoose');
+mongoose 
+  .connect('mongodb://localhost/frienddb')
+  .then(mongo => {
+    console.log('its raining men')
+  })
+  .catch(err => {
+    console.log('error', err);
+  });
+
+const friendController = require('./friends/friendController');
 
 const server = express();
 
@@ -11,6 +24,7 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+server.use('/api/friends', friendController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
