354954427d9b8c6ab303516a8f70898fff3258d9
Yasin Shuman <email@yasin.io>
2018-05-07 16:07:32 -0700

    added logic to check for firstName, lastName and age on post to /api/friends

--- Friends/friendController.js
+++ Friends/friendController.js
@@ -1,31 +1,37 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Friend = require('./friendModel');
+const Friend = require("./friendModel");
 
 router
-	.route('/')
-	.get(get)
-	.post(post);
+  .route("/")
+  .get(get)
+  .post(post);
 
 function get(req, res) {
-	Friend.find().then(friends => {
-		res.status(200).json(friends);
-	});
+  Friend.find().then(friends => {
+    res.status(200).json(friends);
+  });
 }
 
 function post(req, res) {
-	const friendData = req.body;
+  const friendData = req.body;
 
-	const friend = new Friend(friendData);
-
-	friend
-		.save()
-		.then(friend => {
-			res.status(201).json(friend);
-		})
-		.catch(err => {
-			res.status(500).json(err);
-		});
+  const friend = new Friend(friendData);
+  const { firstName, lastName, age } = friend;
+  if (firstName && lastName && age) {
+    friend
+      .save()
+      .then(friend => {
+        res.status(201).json(friend);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: "Please provide firstName, lastName and age for the friend."
+    });
+  }
 }
 
 module.exports = router;
