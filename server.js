0d2ba9debbbe020fdcf59b7db502bf41f83e54b6
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-05-07 13:40:56 -0700

    Initial PR

--- server.js
+++ server.js
@@ -12,5 +12,6 @@ server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
 
+//server
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
