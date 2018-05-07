fa4db227132875f777f990c647908ed086f0e858
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-07 15:15:28 -0400

    connected to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/friends')
+  .then(mongo => console.log("connected to the database"))
+  .catch(err => console.log("error connecting to the database"))
 
 const server = express();
 
@@ -8,9 +13,7 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  res.status(200).json({ api: 'running' });
-});
+server.get('/', (req, res) => { res.status(200).json({ api: 'running' }) });
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
