c0ad674e59f496f648da201fc3e7ca3404a5d3fb
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-05-07 18:00:26 -0500

    initial commit: installed Mongoose & dependencies || set-up base server || laid out file structure

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-const server = express();
+// Mongo Connection
+mongoose
+  .connect('mongodb://localhost/frienddb')
+  .then(mongo => {
+    console.log('connected to database');
+  })
+  .catch(err => {
+    console.log('Error connecting to database', err);
+  });
 
+const friendController = require('./friends/friendController');
+
+// Express
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
@@ -12,5 +24,6 @@ server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
 
+// Server
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
