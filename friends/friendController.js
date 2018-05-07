8c11a8b9a9637fbdb955b15d2e69b4cf18f00459
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-05-07 13:26:03 -0700

    added get, post, delete, and put

---
+++ friends/friendController.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+
+const Friend = require('./friendModel');
+
+router
+  .route('/')
+  .get((req, res) => {
+    Friend
+      .find()
+      .then(friends => {
+        res.status(200).json(friends);
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "The friends information could not be retrieved." })
+      })
+  })
+
+  .post((req, res) => {
+    const friendData = req.body
+    const friend = new Friend(friendData);
+    friend
+      .save()
+      .then(friend => {
+        res.status(201).json(friend);
+      })
+      .catch( err => {
+        res.status(500).json({ errorMessage: "There was an error while saving the friend to the database." });
+      })
+  }) 
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    const id = req.params.id;
+    Friend
+      .findById(id)
+      .then(friends => {
+        res.status(200).json(friends);
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "The friends information could not be retrieved." })
+      })
+  })
+
+  .delete((req, res) => {
+    const id = req.params.id;
+    Friend
+      .findByIdAndRemove(id)
+      .then(friends => {
+        res.status(200).json(friends);
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "The friends information could not be retrieved." })
+      })
+  })
+
+  .put((req, res) => {
+    const id = req.params.id;
+    const friendInfo = req.body;
+    Friend
+      .findByIdAndUpdate(id, friendInfo)
+      .then(friends => {
+        res.status(200).json(friends);
+      })
+      .catch(err => {
+        res.status(500).json({ errorMessage: "The friends information could not be retrieved." })
+      })
+  })
+
+module.exports = router;
