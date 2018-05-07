404c91cbb38660d5114cd3a2115bef2dd5388aa7
Axhon <axhonruiz@gmail.com>
2018-05-06 22:39:07 -1000

    added es6 and morgan

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
 
-if(process.env.NODE_ENV !== 'production'){
- const dotenv = require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  // dev things
+  const dotenv = require('dotenv').config();
 }
 
 const server = express();
-
+server.use(morgan('dev'));
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
