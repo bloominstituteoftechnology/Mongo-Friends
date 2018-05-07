ec02db4514511116a41ab6ca2b7b3219b7bccff0
Eric Gomez <eric_gomez_1@hotmail.com>
2018-05-07 14:34:52 -0700

    working on error handling

--- MVR/friendController.js
+++ MVR/friendController.js
@@ -12,6 +12,8 @@ router.route('/')
   })
 
   .post((req, res) => {
+    if (req.body.firstName === undefined || req.body.lastName === undefined || req.body.age === undefined) res.status(400).json({ errorMessage: "Please provide firstName, lastName and age for the friend." });
+    if (typeof req.body.age !== 'number') res.status(400).json({ errorMessage: "Age must be a number between 1 and 120" });
     friendData = req.body;
     const friend = new Friend(friendData);
     friend.save()
@@ -19,7 +21,7 @@ router.route('/')
         res.status(201).json(friend);
       })
       .catch(err => {
-        res.status(500).json(err);
+        res.status(500).json({ errorMessage: "There was an error while saving the friend to the database." });
       })
   })
 module.exports = router;
