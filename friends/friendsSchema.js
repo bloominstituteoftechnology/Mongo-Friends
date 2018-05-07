d468f4789c32538710e7b1d55e97151412e933b5
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-07 16:36:29 -0400

    add a new friend via post

--- friends/friendsSchema.js
+++ friends/friendsSchema.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
 const Friend = require('./friendsModel');
 
-router.route('/')
+router
+  .route('/')
   .get((req, res) => {
     Friend.find()
       .then(friends => res.status(200).json(friends))
       .catch(err => res.status(500).json({ error: "Cannot retrieve friends." }))
   })
+
+  .post((req, res) => {
+    const friendData = req.body;
+    const friend = new Friend(friendData);
+    friend.save()
+      .then(friend => res.status(200).json(friend))
+      .catch(err => res.status(500).json({ error: "Failed to save friend. Check requirements for new friend." }))
+  });
 
 module.exports = router;
