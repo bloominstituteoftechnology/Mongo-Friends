d34f829feafb79037d7061f529b2e960a274085a
Devon Bacon <devon.bacon@gmail.com>
2018-05-07 13:04:27 -0700

    server.js - created check for db connection

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+// Check to see if mongo is connected
+mongoose
+  .connect('mongodb://localhost/frienddb').then(mongo => {
+    console.log('Connected to database');
+  }).catch(err => {
+    console.log('Error connecting to database', err);
+  });
+
 
 const server = express();
 
@@ -11,6 +21,7 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
