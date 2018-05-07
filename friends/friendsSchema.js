1a037627f0a94b638fb869afc24228abc6aa6a87
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-07 17:43:43 -0400

    update and delete a friend

--- friends/friendsSchema.js
+++ friends/friendsSchema.js
@@ -24,4 +24,19 @@ router.route('/:id')
       .catch(err => res.status(500).json({ error: "Cannot retrieve any friend with the provided ID." }))
   })
 
+  .delete((req, res) => {
+    const { id } = req.params;
+    Friend.findById(id).remove()
+      .then(res => res.status(200).json(res)) // returns catch error but deletes specified friend
+      .catch(err => res.status(500).json({ error: "Cannot delete friend with the provided ID." }))
+  })
+
+  .put((req, res) => {
+    const { id } = req.params;
+    const updatedFriend = req.body;
+    Friend.findByIdAndUpdate(id, updatedFriend)
+      .then(updated => res.status(200).json(updated)) // returns original friend data, still updates
+      .catch(err => res.status(500).json({ error: "Cannot update this friend." }))
+  })
+
 module.exports = router;
