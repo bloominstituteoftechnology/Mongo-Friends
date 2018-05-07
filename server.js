3849f368bc818be056f92d1c86302b035cbbdfa9
David Alexander Morales <damorales89@gmail.com>
2018-05-07 17:14:51 -0400

    mongo running, server.js w/mongoose

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+// connect to mongo
+mongoose
+  .connect('mongodb://localhost/brodb')
+  .then(mongo => {
+    console.log('conneced to database');
+  })
+  .catch(err => {
+    console.log('Error connecting to database', err);
+  });
 
 const server = express();
 
