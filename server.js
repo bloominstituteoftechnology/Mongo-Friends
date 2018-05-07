9b2b31dea579f4138a6df801a7840169e3ba1529
sadrak105 <sadrak10@gmail.com>
2018-05-07 17:08:31 -0400

    setup mongoose

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose
+.connect('mongodb://localhost/friendsdb')
+.then(mongo => {
+  console.log('connected to database');
+})
+.catch(err => {
+  console.log('Error connecting to database', err);
+})
 
 const server = express();
 
@@ -12,5 +22,5 @@ server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
