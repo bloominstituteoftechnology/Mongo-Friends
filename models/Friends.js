867ee497869f27cadd6cae9d94a5a9c01236b47e
Axhon <axhonruiz@gmail.com>
2018-05-06 23:04:27 -1000

    friends model looking good

--- models/Friends.js
+++ models/Friends.js
@@ -1,10 +1,22 @@
-//{
-//  firstName: "Jane", // String, required
-//  lastName: "Doe",  // String, required
-//  age: 18, // Number, required, should be an integer between 1 and 120
-//  createdOn: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, required, defaults to current date
-//}
-import { Schema } from 'mongoose'
+import { Schema, Types, Model } from 'mongoose';
+const { ObjectId } = Types;
 
-export const FriendsSchema = Schema({})
+const FriendTypes = {
+  _id: ObjectId,
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  age: { type: Number, required: true },
+  createdOn: { type: Date, default: Date.now, required: true }
+};
 
+/**
+ * Mongoose Schema for friends document
+ * @type Schema
+ */
+export const FriendsSchema = new Schema(FriendTypes);
+
+/**
+ * Mongoose Model for friends document
+ * @type Model<FriendsSchema>
+ */
+export const FriendsModel = Model('Friends', FriendsSchema);
