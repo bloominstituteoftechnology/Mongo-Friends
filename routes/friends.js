b1125ea0f4ba1203f111e339fe24f7797d96e96c
Chris <cjmarsh725@gmail.com>
2018-05-07 16:24:31 -0700

    Routes for specific ids

--- routes/friends.js
+++ routes/friends.js
@@ -22,39 +22,50 @@ router
       })
       .catch(err => {
         if (err.errors.age)
-          res.status(500).json({ errorMessage: "Age must be a number between 1 and 120" });
+          res.status(400).json({ errorMessage: "Age must be a number between 1 and 120" });
         else if (err.name === "ValidationError")
-          res.status(500).json({ errorMessage: "Please provide firstName, lastName and age for the friend." });
+          res.status(400).json({ errorMessage: "Please provide firstName, lastName and age for the friend." });
         else
-          res.status(500).json(err);
+          res.status(500).json({ errorMessage: "The new friend could not be added." });
       });
   });
 
 router
   .route('/:id')
-  .get((req, res) => {
-    res.status(200).json({ route: '/api/friends/' + req.params.id });
-  })
-  .delete((req, res) => {
-    res.status(200).json({ status: 'please implement DELETE functionality' });
-  })
-  .put((req, res) => {
-    res.status(200).json({ status: 'please implement PUT functionality' });
-  });
-
-function post(req, res) {
-  const friendData = req.body;
-
-  const friend = new Friend(friendData);
-
-  friend
-    .save()
-    .then(friend => {
-      res.status(201).json(friend);
-    })
+  .get((req, res) => { /* GET */
+    Friend
+    .findById(req.params.id)
+    .then(friend => res.status(200).json(friend))
     .catch(err => {
-      res.status(500).json(err);
+      if (err.kind === "ObjectId")
+        res.status(404).json({ message: "The friend with the specified ID does not exist." });
+      res.status(500).json({ errorMessage: "The friend information could not be retrieved." });
     });
-}
+  })
+  .delete((req, res) => { /* DELETE */
+    Friend
+    .findByIdAndRemove(req.params.id)
+    .then(friend => res.status(200).json(friend))
+    .catch(err => {
+      if (err.kind === "ObjectId")
+        res.status(404).json({ message: "The friend with the specified ID does not exist." });
+      res.status(500).json({ errorMessage: "The friend could not be removed" });
+    });
+  })
+  .put((req, res) => { /* PUT */
+    const { firstName, lastName, age } = req.body;
+    if (!firstName || !lastName || !age) 
+      res.status(400).json({ errorMessage: "Please provide firstName, lastName and age for the friend." });
+    else if (isNaN(age) || age < 1 || age > 120)
+      res.status(400).json({ errorMessage: "Age must be a number between 1 and 120" });
+    Friend
+    .findByIdAndUpdate(req.params.id, { firstName, lastName, age })
+    .then(friend => res.status(200).json(friend))
+    .catch(err => {
+      if (err.kind === "ObjectId")
+        res.status(404).json({ message: "The friend with the specified ID does not exist." });
+      res.status(500).json({ errorMessage: "The friend information could not be modified." });
+    });
+  });
 
 module.exports = router;
