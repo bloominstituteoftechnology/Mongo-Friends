c84293d863ae93732341fe214ceab6289782f49a
jaivonmassena <jaivonmassena@gmail.com>
2018-05-07 17:46:43 -0400

    post works

--- friends/friendController.js
+++ friends/friendController.js
@@ -5,8 +5,26 @@ const Friend = require('./friendsDb')
 router
   .route('/')
   .get((req, res) => {
-    Friend.find().then(friends =>{
-    res.status(200).json(friends)})
+    Friend
+      .find()
+      .then(friends =>{
+        res.status(200).json(friends)
+      })
+      .catch(err =>{
+        res.status(500).json(err)
+      })
+  })
+  .post((req, res) =>{
+    const friendData = req.body;
+    const friend = new Friend(friendData)
+    friend
+      .save()
+      .then(friend =>{
+        res.status(200).json(friend)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
   })
 
 
