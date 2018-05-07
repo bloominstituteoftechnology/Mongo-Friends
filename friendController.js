03e7fe6f44b2cddcc70fd9aab5aaab44ebf18c72
Mark Wong <fmarkwong@gmail.com>
2018-05-07 15:46:52 -0700

    update and get one

--- friendController.js
+++ friendController.js
@@ -5,6 +5,18 @@ const Friend = require('./FriendModel');
 
 router.route('/').get(get).post(post);
 
+router.route('/:id')
+  .get((req, res) => {
+    Friend.findById(req.params.id).then(friend => {
+      res.status(200).json(friend);
+    })
+  })
+  .put((req, res) => {
+    Friend.findByIdAndUpdate(req.params.id, req.body, {new: true}).then(friend => {
+      res.status(200).json(friend);
+    })
+  })
+
 function get(req, res) {
   Friend.find().then(friends => {
     res.status(200).json(friends);
