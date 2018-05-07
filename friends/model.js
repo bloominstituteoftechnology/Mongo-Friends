d18a7c9d736575051a87aa6925ace46c9e64eb4d
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-07 15:38:29 -0400

    Created definition

--- friends/model.js
+++ friends/model.js
@@ -1,2 +1,23 @@
 const mongoose = require('mongoose');
 
+const definition = {
+    firstName = {
+        type: String,
+        required: true,
+    },
+    lastName = {
+        type: String,
+        required: true,
+    },
+    age = {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 120,
+    },
+    createdOn = {
+        type: Date,
+        default: Date.now,
+    }
+}
+
