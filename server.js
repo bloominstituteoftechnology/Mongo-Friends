f0e1bf3f1a501ffa3c0429cf66ee16134a804b93
Chris <cjmarsh725@gmail.com>
2018-05-07 14:49:37 -0700

    Wired server for mongoose and routing

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const friendsRoute = require('./routes/friends');
+
+// Connect to mongo
+mongoose
+  .connect('mongodb://localhost/beardb')
+  .then(mongo => {
+    console.log('Connected to database');
+  })
+  .catch(err => {
+    console.log('Error connecting to database', err);
+  });
 
 const server = express();
 
@@ -11,6 +23,8 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
+server.use('/api/friends', friendsRoute);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
