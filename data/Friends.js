87e0092b3d8e547d4e941404aab4cfcf78d5e1e5
Nedu Robert <nedumrobert@gmail.com>
2018-05-07 23:50:52 +0100

    added contactInfo field for friend

--- data/Friends.js
+++ data/Friends.js
@@ -17,7 +17,27 @@ const Friends = new Schema({
     },
     age: {
         type: Number,
+        min: 1,
+        max: 120,
         required: true
+    },
+    contactInfo: {
+        email: {
+            type: String
+        },
+        mobileNumber: {
+            type: Number
+        },
+        githubUserName: {
+            type: String
+        },
+        facebookUserName: {
+            type: String
+        },
+        twitterHandle: {
+            type: String
+        },
+        required: false
     }
 }, options);
 
