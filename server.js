5f12f948e2fd6fd39b31aa32981975de93b7d9c3
Kyle Rennick <kauzlarich.kyle@gmail.com>
2018-05-07 16:00:02 -0700

    get and post done

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const friendsController = require('./friends/friendsController');
+
+mongoose.connect('mongodb://127.0.0.1:27017/firends').then(mongo => {
+  console.log("connected to database");
+}).catch(err => {
+  console.log("error connecting to database", err);
+});
 
 const server = express();
 
@@ -12,5 +20,7 @@ server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
 
-const port = process.env.PORT || 5000;
+server.use('/api/friends', friendsController);
+
+const port = process.env.PORT || 666;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
