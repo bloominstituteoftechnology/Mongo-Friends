4121230388cb1476d64d503a1073ccb9b86800fe
Paul Fang <paulfang@pauls-mbp.home>
2018-05-07 14:21:50 -0700

    server.js complete

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ mongoose
     console.log('Error connecting to database', err);
   });
 
+const friendServer = require('./friend/friendController');
+
 const server = express();
 
 server.use(helmet());
@@ -21,6 +23,8 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
+server.use('/api/friends', friendController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
