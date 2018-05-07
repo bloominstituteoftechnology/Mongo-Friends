c34bccb1b1ee1361725e5cdd7eb247e3cea99eb9
Nedu Robert <nedumrobert@gmail.com>
2018-05-07 23:26:16 +0100

    configured API endpoints to modify friend

--- routes/friends.js
+++ routes/friends.js
@@ -20,7 +20,7 @@ router
         const friend = new Friends(newFriend);
         if (!firstName || !lastName || !age) {
             res.status(400).json({ errorMessage: 'Please provide firstName, lastName and age for the friend.' });
-        } else if (Number.isNaN(age) || (age < 1 || age > 120)) {
+        } else if ((typeof age !== 'number') || (age < 1 || age > 120)) {
             res.status(400).json({ errorMessage: 'Age must be a number between 1 and 120' });
         } else {
             friend
@@ -64,6 +64,50 @@ router
             }
             res.status(500).json({ errorMessage: 'The friend could not be removed ' });
         })
+    })
+    .put((req, res) => {
+        const { id } = req.params;
+        const { firstName, lastName, age } = req.body;
+        const updatedFriend = { firstName, lastName, age };
+        if (!firstName || !lastName || !age) {
+          res
+            .status(400)
+            .json({
+              errorMessage:
+                'Please provide firstName, lastName and age for the friend.',
+            });
+        } else if (typeof age !== 'number' || (age < 1 || age > 120)) {
+            res
+            .status(400)
+            .json({
+                errorMessage:
+                'Age must be a number between 1 and 120',
+            });
+            } else {
+                Friends.findByIdAndUpdate(id, updatedFriend, {
+                runValidators: true,
+                new: true,
+                })
+                .then(friend => {
+                    res.status(200).json(friend);
+                })
+                .catch(err => {
+                    if (err.name === 'CastError') {
+                    res
+                        .status(404)
+                        .json({
+                        errorMessage:
+                            'The friend with the specified ID does not exist.',
+                        });
+                    }
+                    res
+                    .status(500)
+                    .json({
+                        errorMessage:
+                        'The friend information could not be modified.',
+                    });
+                });
+            }
     })
 
 module.exports = router;
