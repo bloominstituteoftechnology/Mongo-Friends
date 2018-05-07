9a9407b60ba737a982ccce5716ee54f532cf58ce
Devon Bacon <devon.bacon@gmail.com>
2018-05-07 13:01:50 -0700

    friendController.js - set root to to get all friends

--- friends/friendController.js
+++ friends/friendController.js
@@ -1,3 +1,17 @@
 const router = require('express').Router();
 
 const Friend = require('./friendModel');
+
+router.route('/')
+  .get((req, res) => {
+    Friend.find()
+      .then(friends => {
+        res.json(friends);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      })
+  })
+
+
+module.exports = router;
