c2fff1704e49a88a6feefedc6cef401be99b8690
jeffrey <jeffreyneilflynn@gmail.com>
2018-05-07 16:41:57 -0400

    get friend by id

--- friends/friendsSchema.js
+++ friends/friendsSchema.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const Friend = require('./friendsModel');
 
-router
-  .route('/')
+router.route('/')
   .get((req, res) => {
     Friend.find()
       .then(friends => res.status(200).json(friends))
@@ -16,5 +15,13 @@ router
       .then(friend => res.status(200).json(friend))
       .catch(err => res.status(500).json({ error: "Failed to save friend. Check requirements for new friend." }))
   });
+
+router.route('/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    Friend.findById(id)
+      .then(friend => res.status(200).json(friend))
+      .catch(err => res.status(500).json({ error: "Cannot retrieve any friend with the provided ID." }))
+  })
 
 module.exports = router;
