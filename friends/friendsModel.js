3d311ccd461a14fec66766d73b5103987d591c36
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-05-08 17:02:32 -0500

    could not get put delete to work properly

--- friends/friendsModel.js
+++ friends/friendsModel.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
 const definition = {
-    firstName: String,
-    lastName: String,
-    age: Number,
+    firstName:{
+        type: String,
+        required: true,
+    },
+    lastName:{
+        type: String,
+        required: true,
+    },
+    age:{
+        type: Number,
+        required: true,
+        min: 1,
+        max: 120,
+    },
     createdOn: {
         type: Date,
         default: Date.now
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const friendsController = require('./friends/friendsController')
+const friendController = require('./friends/friendController')
 
 mongoose
 .connect('mongodb://localhost/frienddb')
@@ -11,6 +11,7 @@ mongoose
 }).catch(err => {
   console.log('error connecting to database', err)
 })
+
 const server = express();
 
 server.use(helmet());
@@ -21,7 +22,9 @@ server.get('/', function(req, res) {
   res.status(200).json({ api: 'running and running' });
 });
 
-// server.use('/api/friends', friendsController);
+server.use('/api/friends', friendController);
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
+server.listen(port, () => {
+ console.log(`\n=== API up on Friends port: ${port} ===\n`)
+});
