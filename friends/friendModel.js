9dbc960d1d9327868a253f400b3f0f1949f66355
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-05-08 17:40:51 -0500

    set-up friendModel.js to schema requirements

---
+++ friends/friendModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const definition = {
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true 
+    },
+    age: {
+        type: Number,
+        required: true,
+        minlength: 1,
+        maxlength: 120
+    },
+    createdOn: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+};
+
+const options = {
+    timestamps: true,
+};
+
+const friendSchema = new mongoose.Schema(definition, options);
+
+const friendModel = mongoose.model('Friend', friendSchema, 'friends');
+
+// Export
+module.exports = friendModel;
