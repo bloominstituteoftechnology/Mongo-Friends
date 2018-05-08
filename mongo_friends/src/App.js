735248a8d99ceb9b0963df2fb1f0c76136e0d0e0
Natalie Schennum <natalieschennum@gmail.com>
2018-05-08 15:35:24 -0700

    Some Styling

--- mongo_friends/src/App.js
+++ mongo_friends/src/App.js
@@ -2,8 +2,27 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import logo from './logo.svg'
 import './App.css';
+import styled from 'styled-components';
 
+const StyledDiv = styled.div`
+width: 600px; 
+height: 100px; 
+border: 2px solid black;
+padding-top: 25px; 
+margin: 0 auto; 
+margin-bottom: 10px;  
+background: white; 
+box-shadow: 3px 5px; 
+font-weight: bolder; 
+&:hover{
+  background: lightblue; 
+  box-shadow: none; 
+}`;
 
+const Li = styled.div`
+padding: 2px; 
+font-size: 20px; 
+}`;
 
 class App extends Component {
   state = {
@@ -35,12 +54,12 @@ class App extends Component {
         <ul>
           {this.state.friends.map(friend => {
             return (
-              <div>
-                <li key={friend.id} className="friend"> </li>
-                <li className="name">{friend.firstName}</li>
-                <li className="name">{friend.lastName}</li>
-                <li className="age">{`Age:${friend.age}`}</li>
-              </div>
+              <StyledDiv>
+                <li key={friend.id} className="friend"></li>
+                <Li className="name">{friend.firstName}</Li>
+                <Li className="name">{friend.lastName}</Li>
+                <Li className="age">{`Age:${friend.age}`}</Li>
+              </StyledDiv>
             )
           })}
         </ul>
