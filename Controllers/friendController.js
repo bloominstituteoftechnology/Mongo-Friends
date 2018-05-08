74d94cee774cc0e313e3e9ebeabf92b272aa74f3
Jonathan Greene <greeneselectronics@gmail.com>
2018-05-08 16:31:47 -0600

    Built out the controller using the stuff from the lecture as a template

--- Controllers/friendController.js
+++ Controllers/friendController.js
@@ -1,25 +1,73 @@
-const mongoose = require('mongoose');
+const router = require('express').Router();
 
-const definition = {
-    friend: {
-        firstName: String,
-        lastName: String,
-        age: Int,
-        createdOn: Date.now,
-    }
+const FriendModel = require('./Models/friendModel');
 
-    
+//pushes the data to the database
+router.post('/', function post(req, res) {
+  const friendData = req.body;
+  const friend = new FriendModel(friendData);
 
-};
+  friend
+    .save()
+    .then(friend => {
+      res.status(201).json(friend);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+//gets all the friends from the database
+router.get('/', function get(req, res) {
+  FriendModel.find().then(friends => {
+    res.status(200).json(friends);
+  });
+});
+
+// gets a specific friend from the database
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+    FriendModel.findById(id)
+    .then(friends => {
+      res.status(200).json(friends);
+    })
+    .catch(error => res.status(500).json(error));
+});
+
+// removes a friend from the database
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Bear.findByIdAndRemove(id)
+    .then(friend => {
+      if (friend) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({ msg: 'Friend does not exist' });
+      }
+    })
+    .catch(error => res.status(500).json(error));
+});
 
 
-const options = {
-    timestamp: true,
-}
+//updates a friend in the database
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const updateFriend = req.body;
 
+  const options = {
+    new: true,
+  };
 
-const friendSchema = new mongoose.Schema(definition, options);
+  FreindModel.findByIdAndUpdate(id, updateFriend, options)
+    .then(friend => {
+      if (friend) {
+        res.status(200).json(friend);
+      } else {
+        res.status(404).json({ msg: 'Friend does not exist' });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
 
-const friendModel = mongoose.model('Friend', friendSchema, 'friends');
-
-module.exports = friendModel;
+module.exports = router;
---
+++ Models/friendModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const definition = {
+    friend: {
+        firstName: String,
+        lastName: String,
+        age: Int,
+        createdOn: Date.now,
+    }
+
+    
+
+};
+
+
+const options = {
+    timestamp: true,
+}
+
+
+const friendSchema = new mongoose.Schema(definition, options);
+
+const friendModel = mongoose.model('Friend', friendSchema, 'friends');
+
+module.exports = friendModel;
