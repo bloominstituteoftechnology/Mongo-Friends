9dcf976ac0b60fb5f0c02903be4327b370c3c4e9
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-08 14:16:33 -0700

    Implement Age & CreatedOn object structure

--- friends/friend.js
+++ friends/friend.js
@@ -2,3 +2,17 @@ const router = require('express').Router();
 
 const Friend = require('./friendModel');
 
+router.post('/', function post(req, res) {
+    const friendData = req.body;
+    const friend = new Friend(friendData);
+
+    friend
+        .save()
+        .then(friend => {
+            res.status(201).json(friend);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
