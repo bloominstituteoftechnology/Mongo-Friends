baf6356cb93be6401f5266dddc3b87e2a5beb769
K Haag <kh@Ks-MacBook-Pro.local>
2018-05-08 13:16:53 -0600

    updates

--- server.js
+++ server.js
@@ -6,12 +6,8 @@ const Friend = require('./Friend.js');
 
 mongoose
 .connect('mongodb://localhost/frienddb')
-.then( mongo => {
-  console.log('Connected to Mongo-I-Friends DB');
-})
-.catch(err => {
-  consol.log('Errof connecting to database', err);
-});
+.then( mongo =>  console.log('Connected to Mongo-I-Friends DB'))
+.catch(err =>  consol.log('Errof connecting to database', err));
 
 const server = express();
 
@@ -22,58 +18,50 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
-});
+})
 
 server.get('/friends', (req, res) => { // GET ALL FRIENDS 
   Friend
   .find()
   .then(friends => res.json(friends))
-  .catch( (err) => res.status(500).json({ error: err }))
-// .catch( () => res.status(500).json({ errorMessage: "The friends information could not be retrieved." }))
-});
-
+  .catch( (err) => res.status(500).json({ errorMessage: "The friends information could not be retrieved." }))
+})
 server.get('/friends/:id', (req, res) => { // GET FRIEND BY ID 
   const { id } = req.params;
   Friend
   .findById(id)
-  .then(friend => !friend ? res.status(404).end() : res.status(200).json(friend)) // DOESNT WORK
-  .catch( (err) => res.status(500).json({ error: err }))
-// .catch( () => res.status(500).json({ errorMessage: "The friends information could not be retrieved." }))
-});
-
-server.post('/friends', (req, res) => {
+  .then(friend => res.status(200).json(friend)) // DOESNT WORK
+  .catch( (err) => err ? res.status(404).json({ error: 'Friend not found' }) : res.status(500).end("Server Error"))
+})
+server.post('/friends', (req, res) => { // POST NEW FRIEND 
   const userInput = req.body;
   console.log('\n POST_FIREND \n', userInput);
   const friend = new Friend(userInput);
-  friend
-  .save()
-  .then(friend => res.status(201, console.log ('\n Successfully created friend \n')).json(friend))
-  .catch( () => { //DOESNT WORK LIKE I WANT IT TOO....
-    if( lastName === undefined || firstName === undefined) {
-    return res.status(400).json({ errorMessage: "Please provide firstName, lastName and age for the friend." })
-  } 
-  else {
-      res.status(500).json(error)
-    }
-  })
+    friend
+      .save()
+      .then(friend => res.status(201, console.log ('\n Successfully created friend \n')).json(friend))
+      .catch( () => { //DOESNT WORK LIKE I WANT IT TOO....
+        if (lastName === null || firstName === undefined) {
+          return res.status(400).json({ errorMessage: "Please provide firstName, lastName and age for the friend." })
+        } 
+        else { res.status(500).json(error) }
+      })
 })
-
-server.delete('/friends/:id', (req, res) => {
+server.delete('/friends/:id', (req, res) => { // DELETE FRIEND BY ID 
     const { id } = req.params;
     let friend;
-    console.log("DELETE_FRIEND", "FRIEND TO DELETE = ", id);
-    Friend
-      .findById(id)
-      .then(foundFriend => { 
-        friend = { ...foundFriend[0] };
-          Friend
-          .findByIdAndRemove(id)
-            .then(response => res.status(200).json(response))
-        })
-        .catch((err) => res.status(500).send({ error: 'Error deleting friend', err }))
+    console.log("FRIEND TO DELETE = ", id);
+      Friend
+        .findById(id)
+        .then(foundFriend => { 
+          friend = { ...foundFriend[0] };
+            Friend
+            .findByIdAndRemove(id)
+              .then(response => res.status(200).json(response))
+          })
+          .catch((err) => res.status(500).send({ error: 'Error deleting friend', err }))
 })
-
-server.put('/friends/:id', (req, res) => {
+server.put('/friends/:id', (req, res) => { // UPDATE FRIEND BY ID 
   const { id } = req.params;
   const update = req.body;
   Friend
@@ -83,13 +71,7 @@ server.put('/friends/:id', (req, res) => {
     })
 })
 
-// function get(req, res) {
-//   Friend
-//   .find()
-//   .then(friends => {
-//     res.status(200).json(friends);
-//   })
-// }
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
