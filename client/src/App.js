497af49d5d602266e6fabe84961766cfc487b4ea
Nedu Robert <nedumrobert@gmail.com>
2018-05-08 01:01:28 +0100

    listed first name of friends

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    friends: []
+  }
+  componentDidMount() {
+    this.getFriends()
+  }
+
+  getFriends = () => {
+    axios.get('http://localhost:5000/api/friends')
+    .then(res => {
+      this.setState({ friends: res.data })
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {this.state.friends.map(friend => <div>{friend.firstName}</div>)}
       </div>
     );
   }
