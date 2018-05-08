40e4bde02b746938883a9725a051fe4bb86778fd
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-05-08 13:58:41 -0700

    Initial Commit

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+// mongod
+// mongo -> show dbs
+// npm i mongoose
+
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+// Connect to Mongo
+mongoose
+  .connect('mongodb://localhost/frienddb')
+  .then(mongo => {
+    console.log('Connected to Database');
+  })
+  .catch(err => {
+    console.log('Error Connecting to Database');
+  });
+
+// const friend = require('./friends/friend');
 
 const server = express();
 
@@ -9,8 +26,12 @@ server.use(cors());
 server.use(express.json());
 
 server.get('/', (req, res) => {
-  res.status(200).json({ api: 'running' });
+  res.status(200).json({ API: 'Running' });
 });
 
+// server.use('/api/friends', friend);
+
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
+server.listen(port, () => {
+  console.log(`\n=== API up on port: ${port} ===\n`);
+});
