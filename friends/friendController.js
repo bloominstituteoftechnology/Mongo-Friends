14045fdc2561e3945c649be6501df972a29ab248
totsuya <supertatortots@gmail.com>
2018-05-08 17:40:43 -0400

    added .put

--- friends/friendController.js
+++ friends/friendController.js
@@ -1,4 +1,38 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Friend = require('./friendsModel');
+const Friend = require("./friendsModel");
 
+router
+  .route("/")
+  .get(get)
+  .post(post);
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    Friend.findbyId(req.params.id)
+      .then(friend => {
+        if (!friend) {
+          res.status(404).json({ error: "No friend under that ID number." });
+        } else {
+          res.status(200).json(friend);
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: "The friend id could not be retrieved." });
+      });
+  })
+
+  .put((req, res) => {
+    const friendData = req.body;
+    if (!friendData.firstName || !friendData.lastName || friendData.age) {
+      return res.status(400).json({ error: "Please fill out all fields." });
+    } else if (age < 1 || age > 120 ) {
+      res.status(400).json({
+        error: "Age must be between 1 to 120."
+      });
+    }
+    
+  });
