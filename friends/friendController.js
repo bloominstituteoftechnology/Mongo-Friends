539c1d45d88568c64a001975c9405d67c840ee41
sadrak105 <sadrak10@gmail.com>
2018-05-08 17:22:49 -0400

    added put

--- friends/friendController.js
+++ friends/friendController.js
@@ -4,17 +4,23 @@ const Friend = require('./friendModel');
 
 //===============POST====================
 router.post('/', (req, res) => {
-    const friendData = req.body;
-    const friend = new Friend(friendData);
-
-    friend
-    .save()
-    .then(friend => {
-        res.status(201).json(friend)
-    })
-    .catch(err => {
-        res.status(500).json({ errorMessage: "There was an error while saving the friend to the database." })
-    }) 
+    if (!(req.body.firstName && req.body.lastName && req.body.age)){
+        res.status(400).json({ errorMessage: "Please provide firstName, lastName and age for the friend." })
+    }
+    if (req.body.age < 1 || req.body.age > 120){
+        res.status(400).json({ errorMessage: "Age must be a number between 1 and 120" })
+    }
+    else {
+        const friend = new Friend(req.body);
+        friend
+        .save()
+        .then(friend => {
+            res.status(201).json(friend)
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "There was an error while saving the friend to the database." })
+        })
+    }
 })
 
 //============GET===============
@@ -60,6 +66,33 @@ router.delete('/:id', (req, res) => {
     })
     .catch(err => {
         res.status(500).json({ errorMessage: "The friend could not be removed" })
+    })
+})
+
+//=================PUT=======================
+router.put('/:id', (req, res) => {
+    const options = {
+        new: true,
+    };
+
+    Friend
+    .findByIdAndUpdate(req.params.id, req.body, options)
+    .then(friend => {
+        if (!(req.body.firstName && req.body.lastName && req.body.age)){
+            res.status(400).json({ errorMessage: "Please provide firstName, lastName and age for the friend." })
+        }
+        if (req.body.age < 1 || req.body.age > 120){
+            res.status(400).json({ errorMessage: "Age must be a number between 1 and 120" })
+        }
+        if (friend){
+            res.status(200).json(friend)
+        }
+        else {
+            res.status(404).json({ message: "The friend with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "The friend information could not be modified." })
     })
 })
 
