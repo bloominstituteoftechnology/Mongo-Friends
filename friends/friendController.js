601b531cee7279eeb269e7415ebe6f99e253e2a4
David Pok <david.pok@gmail.com>
2018-05-07 18:11:49 -0700

    have get requests working, quarter of post requests working

--- friends/friendController.js
+++ friends/friendController.js
@@ -8,6 +8,18 @@ router
     .route('/:id')
     .get((req, res) => {
         res.status(200).json({ route: '/api/friends/' + req.params.id });
+            Friend.findById(req.params.id).then(friends => {
+                res.status(200).json(friend);
+            })
+            .catch(err => {
+                res.status(404).json({ message: "The friend with the specified ID does not exist." })
+            })
+                // Kitten.findById(req.params.kittenId, (err, kitten) => {  
+                //     if (err) return res.status(500).send(err)
+                //     return res.status(200).send(kitten)
+            .catch(err => {
+                res.status(500).json({ errorMessage: "The friend information could not be retrieved." });
+            })
     })
     .delete((req, res) => {
         res.status(200).json({ status: 'what' });
@@ -34,7 +46,7 @@ function post(req, res) {
             res.status(201).json(friend);
         })
         .catch(err => {
-            res.status(500).json(err);
+            res.status(400).json({ errorMessage: "Please provide firstName, lastName and age for the friend." });
         });
 }
 
