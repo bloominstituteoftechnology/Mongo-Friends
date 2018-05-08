8d125e44e3d77d8e6630c76cac35d88aca47a053
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-08 17:55:06 -0400

    Mapped through data and rendered the friends information

--- friendapp/src/App.js
+++ friendapp/src/App.js
@@ -13,9 +13,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get(`localhost:5000/api/friends`)
+    axios.get(`http://localhost:5000/api/friends`)
     .then(res => {
-      console.log(res)
+      this.setState({ friends: res.data })
+      console.log(this.state.friends);
     })
     .catch(err => {
       console.log(err);
@@ -29,7 +30,33 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        
+        {this.state.friends.map(friend => {
+          return <div>
+          <p> {friend.firstName} </p>
+          <p> {friend.lastName} </p>
+          <p> {friend.age} </p>
+          {console.log(friend.contactInfo, "here")}
+          {friend.contactInfo.length > 0 &&
+          <div>
+        <p>
+           {friend.contactInfo[0].email}
+        </p>
+        <p>
+           {friend.contactInfo[0].mobileNumber}
+        </p>
+        <p>
+           {friend.contactInfo[0].githubUsername}
+        </p>
+        <p>
+           {friend.contactInfo[0].facebookUsername}
+        </p>
+        <p>
+           {friend.contactInfo[0].twitterHandle}
+        </p>
+        </div>
+      }
+          </div>
+        })}
       </div>
     );
   }
