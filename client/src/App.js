e2966eb01aeae1aac661b96a3e143e414e6abe61
Nedu Robert <nedumrobert@gmail.com>
2018-05-09 01:04:38 +0100

    added App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,63 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import axios from 'axios';
-import logo from './logo.svg';
+
+import Friends from './Components/Friends/Friends';
+import Friend from './Components/Friend/Friend';
+import Home from './Components/Home/Home';
 import './App.css';
 
 class App extends Component {
-  state = {
-    friends: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: [],
+      firstName: '',
+      lastName: '',
+      age: ''
+    }
   }
+  
   componentDidMount() {
-    this.getFriends()
+    this.getFriends();
   }
 
   getFriends = () => {
     axios.get('http://localhost:5000/api/friends')
     .then(res => {
+      console.log(res);
       this.setState({ friends: res.data })
-    });
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
+
+  handleNewFriend = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  addFriend = (e) => {
+    const friend = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      age: Number(this.state.age),
+    };
+    axios.post('http://localhost:5000/api/friends', friend).then(savedFriend => {
+      console.log(savedFriend);
+      this.setState({ firstName: '', lastName: '', age: '' });
+    }).catch(err => {
+      console.log(err);
+    });
+
+  };
 
   render() {
     return (
       <div className="App">
-      {this.state.friends.map(friend => <div>{friend.firstName}</div>)}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/friends" render={props => <Friends {...props} friends={this.state.friends} handleNewFriend={this.handleNewFriend} addFriend={this.addFriend} state={this.state} />} />
+        <Route exact path="/friends/:id" render={props => <Friend {...props} friends={this.state.friends} getFriends={this.getFriends} />} />
       </div>
     );
   }
