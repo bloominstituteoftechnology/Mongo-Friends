f2d8c97c8cc944f84501b7b332c0d962c1915441
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-31 11:16:43 -0500

    Working on server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/frienddb').then(mongo => {
+  console.log('Connected to database');
+}).catch(err => {
+  console.log("Error while connecting to database", err);
+})
 
 const server = express();
 
@@ -9,10 +16,9 @@ server.use(cors());
 server.use(express.json());
 
 server.get('/', (req, res) => {
+  // 'running' shows up in browser
   res.status(200).json({ api: 'running' });
 });
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
-
-
