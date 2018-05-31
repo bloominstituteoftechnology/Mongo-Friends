f4eeef3112e63df5142dac326a21efa8460df69b
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-05-31 14:35:23 -0500

    Let's see if I can make a post.

---
+++ friends/friendController.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const Friend = require('./friendSchema');
+
+router
+  .route('/')
+  .get(get)
+  .post(post);
+
+router
+  .route('/:id')
+  .get((req, res) => {
+      Friend.find()
+        .then( friends => {
+            res.status(200).json(friends);
+        })
+        .catch( err => {
+            res.status(500).json({error: 'Error getting friends'});
+        })
+  })
+  .post((req, res) => {
+      const friendData = req.body;
+
+      const friend = new Friend(friendData);
+      friend.save()
+        .then( friend => {
+            res.status(201).json(friend);
+        })
+        .catch( err => {
+            res.status(500).json(err);
+        })
+  })
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ mongoose.connect('mongodb://localhost/frienddb').then(mongo => {
   console.log("Error while connecting to database", err);
 })
 
+const friendController = require('./friends/friendController');
 const server = express();
 
 server.use(helmet());
@@ -19,6 +20,8 @@ server.get('/', (req, res) => {
   // 'running' shows up in browser
   res.status(200).json({ api: 'running' });
 });
+
+server.use('/api/friends', friendController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
