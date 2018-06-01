fb787b18bd42c18043d7d5f6f130b3532a103a6b
Victor Montoya <victoramontoya@gmail.com>
2018-05-31 20:42:10 -0700

    almost finished, validators work and erros except for age

--- friends/FriendController.js
+++ friends/FriendController.js
@@ -11,71 +11,72 @@ router
 .get(get)
 .post(post);
 
-// router
-//     .route('/:id')
-//     .get((req, res) => {
-//         const objId = req.params.id;
-//         Bear.findById(objId)
-//             .then(bear => {
-//                 if (bear) {
-//                     res.status(201).json({ bear })
-//                 } else {
-//                     return res.status(404).json({
-//                         message: "The bear with the specified ID does not exist."
-//                     })
-//                 }
-//             })
-//             .catch(err => {
-//                 res.status(500).json({ errorMessage: "The bear information could not be retrieved.", err })
-//             })
-//     })
-//     .delete((req, res) => {
-//         const objId = req.params.id;
-//         Bear.findByIdAndDelete(objId)
-//             .then(deleted => {
-//                 if (deleted) {
-//                     res.status(201).json({ message: "You've deleted the bear", objId })
-//                 } else {
-//                     return res.status(404).json({
-//                         message: "The bear with the specified ID does not exist."
-//                     })
-//                 }
-//             })
-//             .catch(err => {
-//                 res.status(500).json({
-//                     errorMessage: "The bear could not be removed", err
-//                 })
-//             })
-//     })
-//     .put((req, res) => {
-//         const { id } = req.params;
-//         const updatedBear = req.body;
-//         Bear.findByIdAndUpdate(id, updatedBear)
-//             .then((updated => {
-//                 if (updated) {
-//                     res.status(200).json({ updatedBear })
+const msgNotFound = {
+    message: "Your friend with the specified ID does not exist, we hope they dont disappear in real life like this."
+}
 
-//                 } else {
-//                     return res.status(404).json({
-//                         message: "The bear with the specified ID does not exist."
-//                     })
-//                 }
-//             }))
-//             .catch(err => {
-//                 res.status(500).json({
-//                     errorMessage: "The bear could not be updated", err
-//                 })
-//             })
-//     })
+const msgNotExists = {
+    message: "Looking for ghosts? Your friend does not exist on our database, we hope your friend exists in yours"
+}
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        const objId = req.params.id;
+        Friend.findById(objId)
+            .then(friend => {
+                if (friend) {
+                    res.status(201).json({ friend })
+                } else {
+                    return res.status(404).json(msgNotExists)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ errorMessage: "Your friend's information could not be retrieved, try again later."})
+            })
+    })
+    .delete((req, res) => {
+        const objId = req.params.id;
+        Friend.findByIdAndDelete(objId)
+            .then(deleted => {
+                if (deleted) {
+                    res.status(201).json({ message: "You've dropped a friend! We hope it's because you found better ones. Add a new friend now! ", objId })
+                } else {
+                    return res.status(404).json(msgNotExists)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    errorMessage: "The friend could not be removed, if that friend really sucks, please try again later!"
+                })
+            })
+    })
+    .put((req, res) => {
+        const { id } = req.params;
+        const updatedFriend = req.body;
+        Friend.findByIdAndUpdate(id, updatedFriend)
+            .then((updated => {
+                if (updated) {
+                    res.status(200).json({ updatedFriend })
+
+                } else {
+                    return res.status(404).json(msgNotExists)
+                }
+            }))
+            .catch(err => {
+                res.status(500).json({
+                    errorMessage: "Your friend could not be updated"
+                })
+            })
+    })
 
 
-
-function get (req, res) {
+function get(req, res) {
     Friend.find().then( friends => {
         res.status(200).json(friends);
     })
-    .catch(err => {
-        res.status(500).json({ message: "Friends could not be retrieved", err});
+    .catch(err => {0
+        res.status(500).json({ message: "Friends could not be retrieved"});
     })
 }
 
@@ -83,17 +84,22 @@ function post(req, res) {
     const friendData = req.body;
     const friend = new Friend(friendData);
 
-    if (!friendData.firstName || !friendData.lastName || !friendData.age) {
-        return res.status(400).json({
-            errorMessage: "Please provide the name, last name, and age for the homie."
-        })
-    }
+     
+    // if (friendData.age > 120 || friendData.age < 1) {
+    //     return res.status(400).json({
+    //         errorMessage: "Your friend is either too young or too old for you. Age must be a number between 1 and 120!"
+    //     })
+    // }
+    if (!friendData.firstName || !friendData.lastName) {
+        return res.status(400).json({ errorMessage: "Please provide the name, last name, and age for the homie." })
+    } else {
+        }
     friend
         .save().then(friend => {
             res.status(201).json(friend)
         })
         .catch(err => {
-            res.status(500).json({ errorMessage: "There was an error while saving our friend to the database" });
+            res.status(500).json({ errorMessage: "I hope you're better than us at saving your friend. There was an error saving your friend to the database.", err });
         });
 }
 
--- friends/FriendModel.js
+++ friends/FriendModel.js
@@ -1,17 +1,30 @@
 const mongoose = require('mongoose');
 
+function validator(val) {
+    return val;
+}
+
+var exists = [validator, "Please provide the name, last name, and age for the homie."]
+
 const definition = {
     firstName: {
         type: String,
-        required: true,
+        required: [true, "Please provide the name, last name, and age for the homie."],
+        validate: exists
     },
     lastName: {
         type: String,
-        required: true,
+        required: [true, "Please provide the name, last name, and age for the homie."],
+        validate: exists
+
     },
     age: {
         type: Number,
-        required: true,
+        required: [true, "Please provide the name, last name, and age for the homie."],
+        min: [1, 'Your friend is too young for you, age must be between 1 and 120'],
+        max: [120, 'Your friend is too old for you, age must be between 1 and 120'],        
+        validate: exists
+
     },
     createdOn: {
         type: Date,
