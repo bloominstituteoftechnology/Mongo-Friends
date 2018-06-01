a02b22fae1e929b71706e1f6a8b53e8e6311ea2d
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-06-01 17:33:57 -0500

    Unhandled promise rejection taken care of.

--- friends/friendController.js
+++ friends/friendController.js
@@ -10,13 +10,13 @@ router.route('/')
                 res.status(200).json(friends);
             })
             .catch(err => {
-                res.status(500).json({ errorMessage: "The friends information could not be retrieved." })
+                res.status(500).json({ error: "The friends information could not be retrieved." })
             })
     })
     .post((req, res) => {
         const friendData = req.body;
         const friend = new Friend(friendData);
-        console.log(req.body);
+        // console.log(req.body);
         if (!req.body.firstName || !req.body.lastName || !req.body.age) {
             res.status(400).json({ error: "firstName, lastName, and age are required!"});
             return;
@@ -28,7 +28,7 @@ router.route('/')
                 res.status(201).json(friend)
             })
             .catch(err => {
-                res.status(500).json({ errorMessage: "There was an error while saving the friend to the database." });
+                res.status(500).json({ error: "There was an error while saving the friend to the database." });
             });
     });
 
@@ -41,11 +41,11 @@ router.route('/:id')
                 if (friend !== null) {
                     res.status(200).json(friend);
                 } else {
-                    res.status(404).json({ message: "The friend with the specified ID does not exist." });
+                    res.status(404).json({ error: "The friend with the specified ID does not exist." });
                 }
             })
             .catch(err => {
-                res.status(500).json({ errorMessage: "The friend information could not be retrieved." })
+                res.status(500).json({ error: "The friend information could not be retrieved." })
             })
     })
     .delete((req, res) => {
@@ -56,7 +56,7 @@ router.route('/:id')
                 if (deleted !== null) {
                     res.status(200).json(deleted)
                 } else {
-                    return res.status(404).json({ message: "The friend with that ID does not exist" })
+                    return res.status(404).json({ error: "The friend with that ID does not exist" })
                 }
             })
             .catch(err => {
@@ -73,11 +73,11 @@ router.route('/:id')
         Friend.findByIdAndUpdate(friendId, friendData, options)
             .then(friend => {
                 if (friend.firstName === "" || friend.lastName === "" || friend.age === "") {
-                    res.status(400).json({ errorMessage: "Please provide firstName, lastName and age for the friend." })
+                    res.status(400).json({ error: "Please provide firstName, lastName and age for the friend." })
                 } else if (friend !== null) {
                     res.status(200).json(friend);
                 } else {
-                    res.sendStatus(404).json({ message: "The friend with the specified ID does not exist." });
+                    res.sendStatus(404).json({ error: "The friend with the specified ID does not exist." });
                 }
             })
             .catch(err => {
