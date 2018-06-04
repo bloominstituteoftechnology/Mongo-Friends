e896537e4f8932eee8a625f27924bf82b5f12b57
Jacob Henry <jacobiswinning@gmail.com>
2018-06-04 17:23:59 -0600

    Jake Henry Mongo-Friends

--- server.js
+++ server.js
@@ -8,9 +8,19 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.use('/api/friends', bearController)
+
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
 
 const port = process.env.PORT || 5000;
+
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://localhost/dbFriends', {}, err => {
+  if (err) console.log('Database connection failed!', err)
+  console.log('Succcessfully connected to mongo DB')
+})
+
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
