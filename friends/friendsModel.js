4692501bd47f3516c7f8636fad5988b4d3d056cf
William VanDolah <william.vandolah@gmail.com>
2018-06-04 16:52:57 -0500

    complete main

---
+++ friends/friendsModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const FriendSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  
+  lastName: {
+    type: String,
+    required: true,
+  },
+
+  age: {
+    type: Number,
+    required: true,
+  },
+
+  createOn: {
+    type: Date,
+    default: Date.now()
+  },
+})
+
+const friendModel = mongoose.model("Friend", FriendSchema);
+
+module.exports = friendModel;
