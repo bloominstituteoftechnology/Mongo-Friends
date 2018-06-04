fc51866909f957263a61c35daab7b20bb9adbc5f
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-04 18:37:27 -0400

    Created GET endpoint to return all friends

--- routes/friends/getAllFriends.js
+++ routes/friends/getAllFriends.js
@@ -1,5 +1,11 @@
 const Friend = require('../../models/friend');
 
 module.exports = (req, res) => {
-
+  Friend.find()
+    .then(friends => {
+      res.status(200).json(friends);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The friends information could not be retrieved.' });
+    });
 };
