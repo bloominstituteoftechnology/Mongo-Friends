a12b193905b7f709529a681fefe604fe8df65795
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-04 16:54:03 -0500

    implemented GET friend by ID

--- friends/friendController.js
+++ friends/friendController.js
@@ -16,11 +16,13 @@ router
     .post((req, res) => {
         const { firstName, lastName, age, createdOn } = req.body;
         const newFriend = new Friends ({firstName, lastName, age, createdOn });
-        if (!firstName || !lastName || !age){
+        if (!firstName || !lastName || !age) {
             res.status(400).json({error: 'Please provide firstName, lastName and age for the friend.'})
+            return;
         }
         if(age < 1 || age > 120) {
             res.status(400).json({error: 'Age must be a number between 1 and 120'})
+            return;
         }
         newFriend.save()
             .then(savedFriend => {
@@ -28,6 +30,24 @@ router
             })
             .catch(error => {
                 res.status(500).json({error: 'There was an error while saving the friend to the database.'})
+            })
+    })
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        Friends.findById(id) 
+            .then(foundFriend => {
+                if(foundFriend) {
+                    res.json(foundFriend);
+                    return;
+                } else {
+                    res.status(404).json({error: 'The friend with the specified ID does not exist.'})
+                }
+            })
+            .catch(error => {
+                res.status(500).json({error: 'The friend information could not be retrieved.'})
             })
     })
 
