e0256798c86650c45d8df723743cbc54112bd2f5
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-04 19:49:47 -0400

    Created GET endpoint to return a single friend based on id

--- routes/friends/getSingleFriend.js
+++ routes/friends/getSingleFriend.js
@@ -1,5 +1,20 @@
 const Friend = require('../../models/friend');
 
 module.exports = (req, res) => {
-
+  const { id } = req.params;
+  Friend.findById(id)
+    .then(friend => {
+      if(friend){
+        res.status(200).json(friend);
+      } else {
+        res.status(404).json({ error: 'The friend with the specified ID does not exist.' });
+      }
+    })
+    .catch(error => {
+      if(error.name === 'CastError'){
+        res.status(404).json({ error: 'The friend with the specified ID does not exist.' });
+      } else {
+        res.status(500).json({ error: 'The friend information could not be retrieved.' });
+      }
+    });
 };
