1afc8bc81e8e49975a97682642d6838993927406
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-04 16:01:15 -0500

    imported mongoose into server.js & friendModel.js & created friend schema

---
+++ friends/friendModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+// Schema
+// {
+//     firstName: "Jane", // String, required
+//     lastName: "Doe",  // String, required
+//     age: 18, // Number, required, should be an integer between 1 and 120
+//     createdOn: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT) // Date, required, defaults to current date
+//   }
+
+const FriendSchema = new mongoose.Schema({
+    firstName: {
+        type: String, 
+        required: true,
+    }, 
+    lastName: {
+        type: String, 
+        required: true
+    },
+    age: {
+        type: Number,
+        minimum: 1, 
+        maximum: 120
+    },
+    createdOn: {
+        type: Date, 
+        default: Date.now()
+    }
+})
+
+const friendsModel = mongoose.model('Friend', FriendSchema);
+module.exports = friendsModel;
