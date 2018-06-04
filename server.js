ac662d05ea58432231e3e487fec5db2264d5ac4a
Danny Vail <dannyv@dannys-mbp.attlocal.net>
2018-06-04 18:14:55 -0400

    Clean up server file

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const server = require('express')();
 const helmet = require('helmet');
 const cors = require('cors');
 const dbConnection = require('./dbConnection');
-
-const server = express();
+const port = process.env.PORT || 5000;
 
 server.use(helmet());
 server.use(cors());
-server.use(express.json());
+server.use(require('express').json());
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
 
-const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
