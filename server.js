5a5854b6a7745487bf06163000b2b2d20539814b
mister-corn <code@ronaldlibago.com>
2018-06-04 12:57:59 -0700

    Friend Schema & Model Established

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const server = express();
 
 server.use(helmet());
@@ -10,6 +10,13 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
+});
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/dbFriends', {}, err => {
+  if (err) console.log(err);
+  console.log('Mongoose connected us to our DB');
 });
 
 const port = process.env.PORT || 5000;
