d51c95f8bff946f8f27fc458c61d667a5d11383b
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-04 16:43:25 -0500

    implemented POST friend

--- friends/friendController.js
+++ friends/friendController.js
@@ -13,6 +13,22 @@ router
                 res.status(500).json({error: 'The friends information could not be retrieved.'})
             })
     })
-
+    .post((req, res) => {
+        const { firstName, lastName, age, createdOn } = req.body;
+        const newFriend = new Friends ({firstName, lastName, age, createdOn });
+        if (!firstName || !lastName || !age){
+            res.status(400).json({error: 'Please provide firstName, lastName and age for the friend.'})
+        }
+        if(age < 1 || age > 120) {
+            res.status(400).json({error: 'Age must be a number between 1 and 120'})
+        }
+        newFriend.save()
+            .then(savedFriend => {
+                res.status(201).json(savedFriend);
+            })
+            .catch(error => {
+                res.status(500).json({error: 'There was an error while saving the friend to the database.'})
+            })
+    })
 
     module.exports = router;
