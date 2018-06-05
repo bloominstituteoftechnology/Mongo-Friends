2ef82e04eb75250826d5a3ecc53fc24a2adee570
bbenefield89 <bsquared18@gmail.com>
2018-06-05 18:27:32 -0400

    feat(client/package.json): add proxy property feat: Displays data on the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import Friends from './components/Friends/Friends';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +9,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        <Friends />
+          
       </div>
     );
   }
---
+++ client/src/components/Friends/Friends.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Friend from './Friend';
+
+const Friends = () => {
+  return <Friend />;
+}
+ 
+export default Friends;
