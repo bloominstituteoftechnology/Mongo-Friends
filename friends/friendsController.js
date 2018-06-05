0c187c8a31b377d4e2f030b113bfa6021afc040d
sagardesai90 <saagardesai@gmail.com>
2018-06-05 18:04:31 -0500

    re did the status codes

--- friends/friendsController.js
+++ friends/friendsController.js
@@ -16,12 +16,14 @@ router
         const {firstName, lastName, age} = req.body;
         const newFriend = new Friend ({firstName, lastName, age});
         if(!firstName || !lastName || !age){
-            res.status(400).json({error: "Please provide first name, last name, and age."})
+            res.status(400).json({errorMessage: "Please provide first name, last name, and age."})
+        } else if(age < 1 || age > 120){
+            res.status(400).json({errorMessage: "Age must be a number between 1 and 120"})
         }
         newFriend
         .save()
         .then(savedFriend => {
-            res.status(200).json(savedFriend)
+            res.status(201).json(savedFriend)
         })
         .catch(err => {
             res.status(500).json({error: "There was an error while saving to the db."})
@@ -38,7 +40,7 @@ router
         Friend.findById(id)
         .then(friend => {
             if (friend === null){
-                res.status(404).json({error: "ID cannot be found."})
+                res.status(404).json({error: "The friend with the specified ID does not exist."})
                 return;
             } else {
                 console.log(friend.id.length);
@@ -46,18 +48,18 @@ router
             }
         })
         .catch(err => {
-            res.status(500).json({error: 'Something has gone wrong, please try again.'})
+            res.status(500).json({error: 'The friend information could not be retrieved.'})
         })
     })
     .delete((req, res) => {
         const {id} = req.params;
         if (id.length !== 24 || typeof id === String || typeof id === Number){
-            res.status(400).send({error: "Something has gone wrong, please try again."})
+            res.status(400).send({error: "IDs need to be 24 chars long."})
             return;
         }
         Friend.findByIdAndRemove(id)
         .then(response => {res.status(200).json(response)})
-        .catch(err => {res.status(500).json({error: "Something has gone wrong, please try again."})});
+        .catch(err => {res.status(500).json({ errorMessage: "The friend could not be removed" })});
     })
     .put((req, res) => {
         const {id} = req.params;
@@ -68,10 +70,12 @@ router
         console.log(friend);
         if(id.length !== 24 || typeof id === String || typeof id === Number){
             res.status(400).json({error: "The ID you have entered is in an incorrect format."})
+        } else if(age < 1 || age > 120){
+            res.status(400).json({ errorMessage: "Age must be a number between 1 and 120." })
         } else {
             Friend.findByIdAndUpdate(id, friend)
             .then(friend => {res.status(200).json(friend)})
-            .catch(err => {res.status(404).json({error: "No friend has this ID in the db."})})
+            .catch(err => {res.status(500).json({ errorMessage: "The friend information could not be modified." })})
         }
     });
 module.exports = router;
