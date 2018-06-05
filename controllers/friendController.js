86ee491c91d0d0d47b0ff3db7a1fd6815c7e85a3
Brad Traversy <mercado7946@gmail.com>
2018-06-05 14:48:06 -0700

    initial commit

---
+++ controllers/friendController.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const Friend = require('../models/friend');
+
+router
+    .route('/')
+    .get((req, res) => {
+       Friend.find()
+        .then(friends => {
+            res.status(200).json(friend);
+        })
+        .catch(err => res.status(500).json({error: 'Error fetching friends'}))
+    })
+
+    module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const server = express();
+
+const friendController = require('./controllers/friendController');
 
 server.use(helmet());
 server.use(cors());
@@ -12,5 +15,15 @@ server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
 
-const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
+server.use('./api/friends', friendController);
+
+const port = process.env.PORT || 5005;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/dbFriends', {}, err => {
+  console.log('Mongoose connected us to our DB');
+});
+
+server.listen(port, () => {
+  console.log(`\n=== API up on port: ${port} ===\n`)
+});
