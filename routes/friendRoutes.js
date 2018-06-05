d78618a4b7d337749c06faac1a70965d9ed39e98
bbenefield89 <bsquared18@gmail.com>
2018-06-05 03:04:36 -0400

    mv friendSchema.js database/friendSchema.js mv friendRoutes.js routes/friendRoutes.js feat(error.js): helper function to handle API errors fix(server.js): change path names for require variables

--- routes/friendRoutes.js
+++ routes/friendRoutes.js
@@ -1,68 +1,80 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
 
-const Friend = require('./friendSchema');
+const Friend = require('../database/friendSchema');
+const error = require('./helpers/error');
 
+/*************************
+** ROUTE / **
+*************************/
 router
+  // get
   .get('/', (req, res) => {
     Friend.find({}, (err, dbRes) => {
       if (err)
-        return res.status(500).json({ err });
+        return error(res, 500, 'ERROR');
       if (dbRes.length === 0)
-        return res.status(404).json({ err: 'Looks like you dont have any friends' });
+        return error(res, 404, 'Looks like you dont have any friends');
       res.json(dbRes);
     })
   })
+  // post
   .post('/', (req, res) => {
     const { firstName, lastName, age } = req.body;
     if (!firstName || !lastName || !age)
-      return res.json({ err: 'Please provide a first and last name as well as an age' });
+      return error(res, 400, 'Please provide a first and last name as well as an age');
     if (age < 1 || age > 120)
-      return res.json({ err: 'A friends age must be between 1 and 120' });
+      return error(res, 400, 'A friends age must be between 1 and 120');
     const friend = { firstName, lastName, age }
     const newFriend = new Friend(friend);
     newFriend.save({}, (err, dbRes) => {
       if (err)
-        return res.status(500).json({ err });
+      return error(res, 500, 'ERROR');
       res.json(dbRes);
     });
   });
 
+/*************************
+** ROUTE /:id **
+*************************/
 router
+  // get
   .get('/:id', (req, res) => {
     const { id } = req.params;
     Friend.findById(id, (err, dbRes) => {
       if (err)
-        return res.status(500).json({ err });
+        return error(res, 500, 'ERROR');
       if (!dbRes)
-        return res.status(404).json({ err: 'That friend doesnt exist' });
+        return error(res, 404, 'That friend doesnt exist');
       res.json(dbRes);
     });
   })
+  // delete
   .delete('/:id', (req, res) => {
     const { id } = req.params;
     Friend.findByIdAndDelete(id, (req, res) => {
       if (err)
-        return res.status(500).json({ err });
+        return error(res, 500, 'ERROR');
       if (dbRes.length === 0)
-        return res.status(404).json({ err: 'That friend doesnt exist' });
+        return error(res, 404, 'That friend doesnt exist');
       res.json(dbRes);
     });
   })
+  // put
   .put('/:id', (req, res) => {
     const { id } = req.params;
     const { firstName, lastName, age } = req.body;
     Friend.findById(id, (err, raw) => {
       if (err)
-        return res.status(500).json({ err });
+        return error(res, 500, 'ERROR');
       if (!raw)
-        return res.status(404).json({ err: 'That friend doesnt exist' });
+        return error(res, 404, 'That friend doesnt exist');
       raw.firstName = firstName || raw.firstName;
       raw.lastName  = lastName || raw.lastName;
       raw.age       = age || raw.age;
       raw.save((err, newRaw) => {
         if (err)
-          return res.status(500).json({ err });
+          return error(res, 500, 'ERROR');
         res.json(newRaw);
       });
     })
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-const friendAPI = require('./friendRoutes');
+const friendAPI = require('./routes/friendRoutes');
 
 const server = express();
 
