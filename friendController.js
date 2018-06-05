1df0bc39e5c85ddb3cda1943769ea3e9e2e52bd1
AmyShackles <amyshackles@gmail.com>
2018-06-04 21:54:35 -0700

    Created route for '/:id'
    - Made post request that returns a 400 error if someone does not include firstName, lastName, and/or age
    - It also returns a 400 error if the age is not a number, is less than 1, or is larger than 120.
    - Made a get request that searches by ID and then returns the foundFriend
    - Realized after making delete request that it would return a null value for foundFriend' if the friend had already been deleted, so added a 404 for if the foundFriend was equivalent to null.
    - Altered delete request so that it also has a if statement to test whether or not its variable returned is null.
    - Although update method returns the updatedFriend, there is no need to add an additional 404 for that method as it is covered by the get method's 404.
    - For all other 404 status, I have a server.use handling it in my server.js file.

--- friendController.js
+++ friendController.js
@@ -18,7 +18,7 @@ router
     .post((req, res) => {
         const { firstName, lastName, age } = req.body;
         const newFriend = new Friend({ firstName, lastName, age });
-        if (!firstName || !lastName) {
+        if (!firstName || !lastName || !age) {
             sendUserError(400, "Please provide firstName, lastName, and age for the friend.", res)
         } else if (typeof age !== 'number' || age > 120 || age < 1) {
             sendUserError(400, "Age must be a number between 1 and 120", res)
@@ -32,6 +32,53 @@ router
                     sendUserError(500, "There was an error while saving the friend to the database.", res)
                 })
         }
+    })
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        Friend.findById(id)
+            .then(foundFriend => {
+                if (foundFriend !== null) {
+                res.status(200).json({ foundFriend })
+            } else {
+                sendUserError(404, "This friend is no longer in our database.", res)
+            }})
+            .catch(err => {
+                sendUserError(500, "The friends information could not be retrieved.", res)
+            });
+    })
+    .delete((req, res) => {
+        const { id } = req.params;
+        Friend.findByIdAndRemove(id)
+            .then(deletedFriend => {
+                if (deletedFriend !== null) {
+                res.status(200).json({ deletedFriend })
+            } else {
+                sendUserError(404, "This friend is no longer in our database and could not be removed.", res)
+            }})
+            .catch(err => {
+                sendUserError(500, "The friend could not be removed.", res)
+            })
+    })
+    .put((req, res) => {
+        const { id } = req.params;
+        const { firstName, lastName, age } = req.body;
+        if (!firstName || !lastName) {
+            sendUserError(400, "Please provide firstName, lastName, and age for the friend.", res)
+        } else if (typeof age !== 'number' || age > 120 || age < 1) {
+            sendUserError(400, "Age must be a number between 1 and 120", res)
+        } else {
+             Friend.findByIdAndUpdate(id, { firstName, lastName, age }, { new: true })
+                .then(updatedFriend => {
+                    res.status(200).json({ updatedFriend })
+                })
+                .catch(err => {
+                    sendUserError(500, "The friend information could not be modified.", res)
+                })
+        }
+       
     })
 
 
