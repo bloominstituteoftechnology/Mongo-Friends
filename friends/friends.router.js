da539e352cd7cd05430dfb022f8e3d02c2bcc5d7
jesuarva <jean.ariza@jesuarva.com>
2018-06-05 21:55:21 +0200

    DEFINE Error handlers

--- friends/friends.router.js
+++ friends/friends.router.js
@@ -12,4 +12,27 @@ routerFriends
   .put()
   .delete();
 
+routerFriends.use(handleError);
+
+/**
+ * ROUTER HANDLERS: handle endpoints
+ */
+
+/**
+ * ERROR: Handle Error
+ */
+function handleError(err, req, res, next) {
+  !err.status ? next(err) : res.status(err.status).json(err.message);
+  next();
+}
+//This function add some properties to an Error object
+function createError(code = 500, message = 'Ups, there were a problem fetching the info from the database') {
+  let e = new Error();
+  e.status = code;
+  e.message = message;
+  return e;
+}
+/**
+ * MIDDLEWARES: Custom middlewears
+ */
 module.exports = routerFriends;
