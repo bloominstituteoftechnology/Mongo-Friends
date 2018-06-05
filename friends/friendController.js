18994c4ef7c0dd53650fe03a224619729d92d632
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-06 01:25:33 +0700

    added get to friendController.js

---
+++ friends/friendController.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const Friend = require('./friendModel');
+
+router
+  .route('/')
+  .get((req, res) => {
+    Friend.find()
+      .then(friends => {
+        res.status(200).json(friends);
+      })
+      .catch(err => {
+        res.status(500).json({ error: "The friend information could not be retrieved." });
+      })
+  });
