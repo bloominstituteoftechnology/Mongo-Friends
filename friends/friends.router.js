f746f9b72c0271120982fc6fd32a6b3b15a2f583
jesuarva <jean.ariza@jesuarva.com>
2018-06-05 22:29:26 +0200

    ADD POST Error handling if there are missing parameters

--- friends/friends.router.js
+++ friends/friends.router.js
@@ -4,7 +4,7 @@ const Friend = require('./friends.model');
 routerFriends
   .route('/')
   .get()
-  .post();
+  .post(handlePOST);
 
 routerFriends
   .route('/:id')
@@ -17,15 +17,29 @@ routerFriends.use(handleError);
 /**
  * ROUTER HANDLERS: handle endpoints
  */
+function handlePOST(req, res, next) {
+  const data = ({ firstName, lastName, age } = req.body);
+  if (!firstName || !lastName || !age)
+    // create an error and past it to "handleError"
+    next(createError(400, 'Please provide firstName, lastName and age for the friend.'));
 
+  const newFriend = new Friend(data);
+  newFriend
+    .save()
+    .then(response => {
+      res.status(200).json();
+    })
+    .catch(e => {
+      next(e);
+    });
+}
 /**
  * ERROR: Handle Error
  */
 function handleError(err, req, res, next) {
-  !err.status ? next(err) : res.status(err.status).json(err.message);
+  !err.status ? next(err) : res.status(err.status).json({ errorMessage: err.message });
   next();
 }
-//This function add some properties to an Error object
 function createError(code = 500, message = 'Ups, there were a problem fetching the info from the database') {
   let e = new Error();
   e.status = code;
