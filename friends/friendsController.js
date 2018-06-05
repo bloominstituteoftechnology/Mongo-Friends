19c26bc648b12e8adc49669585662e8f48541612
kammagik <kammagik@yahoo.com>
2018-06-05 16:14:15 -0700

    PUT done

--- friends/friendsController.js
+++ friends/friendsController.js
@@ -5,8 +5,8 @@ router
 .route('/')
 .get((req, res) => {
  Friend.find()
-    .then(friends => {
-        res.status(200).json(friends)
+    .then(friend => {
+        res.status(200).json(friend)
     })
     .catch(err => res.status({error: 'Error fetching friends'}));
 })
@@ -59,7 +59,25 @@ router
     })
 })
 .put((req, res) => {
-    
+    const { id } = req.params;
+    const updates = ({ firstName, lastName, age} = req.body);
+    if(!firstName || !lastName || !age) {
+        res.status(400).json({ errorMessage: 'Please provide firstName, lastName and age for the friend.'});
+        return;
+    }
+        else (isNaN(age) || age < 1 || age > 120) 
+            res.status(400).json({ errorMessage: 'Age must be a number between 1 and 120'});
+            return;
+        
+    Friend.findByIdAndUpdate(id, updates, {new: true})
+    .then(friendUpdated => {
+        if (friendUpdated === null) {
+            res.status(404).json({ errorMessage:'The friend could not be removed'});
+            return;
+        }
+        res.json(friend)
+    })
+    .catch (err => res.status(500).json({ errorMessage:'The friend information could not be modified.'}))
 })
 
 module.exports = router;
