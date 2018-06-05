d61d09067960c549e9444d59d0cbae2bac02be35
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-06-05 15:37:48 -0700

    reorganized server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const friendController = require('./friends/friendController.js');
 const localHost = 'localhost:27017';
 const database = 'frienddb';
+const server = express();
+const port = process.env.PORT || 5000;
 
 mongoose
     .connect(`mongodb://${localHost}/${database}`)
@@ -14,22 +16,14 @@ mongoose
     .catch(error => {
         console.log("Connection Failed")
     });
-    
-const friendController = require('./friends/friendController.js');
-
-const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use('/api/friends', friendController);
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
-
-server.use('/api/friends', friendController);
-
-
-const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
