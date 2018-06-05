6a78d452561df2607681125693ed638024030da5
jaklundun <govi.escudero@gmail.com>
2018-06-05 15:23:52 -0700

    Wrote DELETE by id endpoint

--- friends/friendController.js
+++ friends/friendController.js
@@ -32,10 +32,47 @@ router
                 res.status(201).json(friend); //201: created 
             })
             .catch(err => {
-                res.status(422).json({error: err}) //422: Unproc­essable Entity
+                res.status(422).json({error: err}); //422: Unproc­essable Entity
             })
         }
-    })
+    });
+
+    router
+        .route('/:id')
+            .get((req, res) => {
+                const { id } = req.params;
+                Friends.findById(id)
+                .then(singleFriend => {
+                    res.json(singleFriend);
+                })
+                .catch(err => {
+                    if(err.name === "CastError") {
+                        res.status(404).json({error: 'The friend with the specified ID doesn\'t exist.'});
+                        return;
+                    } else {
+                        res.status(500).json({error: 'The friend could not be removed'});
+                    }
+                })
+            })
+            .delete((req, res) => {
+                const { id } = req.params;
+                Friends.findByIdAndRemove(id)
+                .then(removedFriend => {
+                    res.json(removedFriend); 
+                })
+                .catch(err => {
+                    if (err.name === null) {
+                          // IF there is no friend by that id, then mongo won't throw an error, rather give us back a null object
+                        // MAKE SURE you handle this null object!!!
+                        res.status(404).json({error: 'The friend with the specified ID does not exist'});
+                        return;
+                    } 
+                        res.status(500).json({error: 'The friend could not be removed'});
+                })
+            })
+
+
+
 
 module.exports = router; 
 
