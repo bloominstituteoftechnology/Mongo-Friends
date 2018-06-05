8803e2d423d90a65eb61fc58172a4276b9728e98
A99 <A99@99s-macbook-pro.home>
2018-06-05 22:36:15 +0200

    Added all CRUD operations

--- friends/friendModel.js
+++ friends/friendModel.js
@@ -1,7 +1,22 @@
 const mongoose = require('mongoose');
 
 const FriendSchema = new mongoose.Schema({
-        //edit here
+    firstName: {
+            type: String,
+            required: true
+    },
+    lastName: {
+            type: String,
+            required: true
+    },
+    age: {
+            type: Number,
+            required: true
+    },
+    createdOn: {
+            type: Date,
+            default: Date.now()
+    }
 });
 
 const friendsModel = mongoose.model('Friend', FriendSchema);
