1c4223ad8390994250942c0535a6ad7b438e7c50
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-05 08:18:56 -0500

    Project initialized
    Mongoose connected to DB. Contoller and Model files created. Server.js file completed.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const server = express();
+
+const friendController = require('./friends/friendController');
 
 server.use(helmet());
 server.use(cors());
@@ -13,4 +15,12 @@ server.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
+
+mongoose.Promise = global.Promise;// configure the mongoose promise to use Native JS Promises
+mongoose.connect('mongodb://localhost/dbFriends', {}, err => {
+  // declare where we're going to connect this is the equivilent of using 'use dbBears' int the mongo shell
+  if (err) console.log(err);
+  console.log('Mongoose connected us to our DB');
+});
+
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
