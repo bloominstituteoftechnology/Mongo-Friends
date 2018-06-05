ec0f21451091d44f108861e1a2aa2fc8eea65000
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-06-05 15:46:40 -0400

    error messages updated

--- friends/friendController.js
+++ friends/friendController.js
@@ -7,7 +7,7 @@ router
   .get((req, res) => {
     Friend.find()
       .then(friends => {
-        res.status(200).json(friends);
+        res.json(friends);
       })
       .catch(err => res.status(500).json({ error: 'Error fetching friends' }));
   })
@@ -25,47 +25,55 @@ router
   });
 
 router
-  .route('/:id')
-  .get((req, res) => {
-    const { id } = req.params;
-    Friend.findById(id)
-      .then(foundFriend => {
-        res.status(200).json(foundFriend);
-      })
-      .catch(err => {
-        res.status(404).json({ error: 'No friend by that id in DB' });
-      });
-  })
-  .delete((req, res) => {
-    const { id } = req.params;
-    Friend.findByIdAndRemove(id)
-      .then(friend => {
-        if (friend === null) {
-            res.status(404).json({ errorMessage: "The friend with the specified ID does not exist." })
-          } else {
-            res.status(200).json(friend);
-          }
-        })
-      .catch(err => {
-        res.status(500).json({ error: 'No friend by that id in DB' });
-      })
-  })
-  .put((req, res) => {
-    const { id } = req.params;
-    const friend = ({ firstName, lastName, age, contactInfo } = req.body);
-    Friend.findByIdAndUpdate(id, friend)
-      .then(friend => {
+    .route('/:id')
+    .get((req, res) => {
+        const { id } = req.params;
         Friend.findById(id)
-        .then(foundFriend => {
-          res.status(200).json(foundFriend);
+            .then(foundFriend => {
+                if (foundFriend === null) {
+                    res.status(404).json({ errorMessage: "The friend with the specified ID does not exist." })
+                } else {
+                    res.status(200).json(foundFriend);
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'Error reading the DB' });
+            });
+    })
+    .delete((req, res) => {
+        const { id } = req.params;
+        Friend.findByIdAndRemove(id)
+        .then(friend => {
+            if (friend === null) {
+                res.status(404).json({ errorMessage: "The friend with the specified ID does not exist." })
+            } else {
+                res.status(200).json(friend);
+            }
         })
         .catch(err => {
-          res.status(404).json({ error: 'The friend with the specified ID does not exist.' });
-        });
-      })
-      .catch(err => {
-        res.status(404).json({ error: 'No friend by that id in DB' });
-      })
-  });
+            res.status(500).json({ error: 'No friend by that id in DB' });
+        })
+    })
+    .put((req, res) => {
+        const { id } = req.params;
+        const friend = ({ firstName, lastName, age, contactInfo } = req.body);
+        Friend.findByIdAndUpdate(id, friend)
+        .then(friend => {
+            if (friend === null) {
+                res.status(404).json({ errorMessage: "The friend with the specified ID does not exist." })
+            } else {
+                Friend.findById(id)
+                .then(foundFriend => {
+                    res.json(foundFriend);
+                })
+                .catch(err => {
+                    res.status(404).json({ error: 'The friend with the specified ID does not exist.' });
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Error updating the DB' });
+        })
+    });
 
 module.exports = router;
