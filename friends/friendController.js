cb959435d83daa44b7754d398d36227068daaa78
A99 <A99@99s-macbook-pro.home>
2018-06-05 22:39:39 +0200

    Added all CRUD operations

--- friends/friendController.js
+++ friends/friendController.js
@@ -2,7 +2,73 @@ const router = require('express').Router();
 
 const Friend = require('./friendModel.js');
 
+const sendUserError = (status, message, res) => {
+    res.status(status).json({ errorMessage: message });
+}
+
 router
-    //start here
+    .route('/')
+    .get((req, res) => {
+        Friend
+            .find()
+            .then(response => {
+                res.json(response);
+            })
+            .catch(err => {
+                return sendUserError(500, err, res);
+            });
+    })
+    .post((req, res) => {
+        const newFriend = new Friend({ firstName, lastName, age } = req.body);
+        newFriend
+            .save()
+            .then(response => {
+                res.status(201).json(response);
+            })
+            .catch(err => {
+                return sendUserError(500, err, res);
+            });
+    });
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        Friend
+            .findById(id)
+            .then(response => {
+                if(response === null) return sendUserError(404, `The friend with the ID ${id} does not exist`, res);
+                res.json(response);
+            })
+            .catch(err => {
+                return sendUserError(500, err, res);
+            });
+    })
+    .delete((req, res) => {
+        const { id } = req.params;
+        Friend
+            .findByIdAndDelete(id)
+            .then(response => {
+                if(response === null) return sendUserError(404, `The friend with the ID ${id} does not exist`, res);
+                res.json(response);
+            })
+            .catch(err => {
+                return sendUserError(500, err, res);
+            });
+    })
+    .put((req, res) => {
+        const { id } = req.params;
+        const updatedFriend = ({ firstName, lastName, age } = req.body);
+        Friend
+            .findByIdAndUpdate(id, updatedFriend, {new: true})
+            .then(response => {
+                if(response === null) return sendUserError(404, `The friend with the ID ${id} does not exist`, res);
+                res.json(response);
+            })
+            .catch(err => {
+                return sendUserError(500, err, res);
+            });
+    });
+    
 
 module.exports = router;
