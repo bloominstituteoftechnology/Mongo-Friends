84464b7f1f83903cdd925721513d75424264e5a8
Brad Traversy <mercado7946@gmail.com>
2018-06-05 15:59:40 -0700

    added CRUD operationS

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -12,4 +12,57 @@ router
         .catch(err => res.status(500).json({error: 'Error fetching friends'}))
     })
 
+    .post((req, res) => {
+        const { firstName, lastName, age } = req.body;
+        const newFriend = new Friend({ firstName, lastName, age });
+        newFriend
+            .save()
+            .then(savedFriend => {
+                res.status(201).json(savedFriend);
+            })
+            .catch(err => {
+                res.status(422).json({ error: 'Error saving friends'});
+            });
+    });
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        Friend.findById(id)
+            .then(foundFriend => {
+                if (foundFriend === null) {
+                    res.status(404).json({ error: 'You don\'t have a friend at least in DB'});
+                };
+            });
+    })
+
+    .delete((req, res) => {
+        const { id } = req.params;
+        Friend.findByIdAndRemove(id)
+            .then(removeFriend => {
+                console.log(exterminatedFriend);
+                if (exterminatedFriend === null) {
+                    res.status(404).json({ error: 'No friend found in DB to exterminate'});
+                    return;
+                }
+                res.json({ success: `Friend exterminated. Qapla!`, resources: exterminatedFriend})
+            })
+            .catch(err => res.status(500).json({ error: err}));
+    })
+
+    .put((req, res) => {
+        const { id } = req.params;
+        const update = ({ firstName, lastName, age } = req.body);
+        Friend.findByIdAndUpdate(id, update, {new: true})
+            .then(friendUpdated => {
+                if (friendUpdated === null) {
+                    res.status(404).json({ error: 'No friend found in DB to exterminate'});
+                    return;                    
+                }
+                res.json({ success: 'Updated the friend', resource:friendUpdated});
+            })
+            .catch(err => res.status(500).json({ error: err }));
+    });
+
     module.exports = router;
