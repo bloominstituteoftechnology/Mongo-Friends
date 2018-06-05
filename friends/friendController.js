c39ec1868aaae11c1c3c29eea3fac6b3e9f65d01
udykas <udykas@gmail.com>
2018-06-05 16:37:52 -0400

    Can now GET friends by ID

--- friends/friendController.js
+++ friends/friendController.js
@@ -35,4 +35,21 @@ router
             })
     });
 
+router
+    .route('/:id')
+    .get((req, res) => {
+        Friend
+            .findById(req.params.id)
+            .then(friend =>{
+                if(!friend){
+                    res.status(404).json({ error: "The friend with the specified ID does not exist." })
+                } else{
+                    res.status(200).json(friend);
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ error: "The friend information could not be retrieved." })
+            })
+    })
+
 module.exports = router;
