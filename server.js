1c89e75b3f770fc54d441d4452620cd897c4288d
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-06-05 15:29:44 -0700

    moved mongoose.connect into server

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const localHost = 'localhost:27017';
+const database = 'frienddb';
+
+mongoose
+    .connect(`mongodb://${localHost}/${database}`)
+    .then(response => {
+        console.log("Connection Successful")
+    })
+    .catch(error => {
+        console.log("Connection Failed")
+    });
+    
 const friendController = require('./friends/friendController.js');
 
 const server = express();
@@ -17,17 +29,7 @@ server.get('/', (req, res) => {
 
 server.use('/api/friends', friendController);
 
-const port = process.env.PORT || 5000;
 
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(
-    'mongod://localhost/dbFriends',
-    {},
-    err => {
-      if (err) console.log(err);
-      console.log('Mongoose Connection to Database Succesfull')
-    }
-  );
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
