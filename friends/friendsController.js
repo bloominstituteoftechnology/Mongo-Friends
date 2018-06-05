bb3e6ef164d26e4791ff14faac8f9334d63558d2
kammagik <kammagik@yahoo.com>
2018-06-05 13:33:06 -0700

    setup friendsController and server.js

---
+++ friends/friendsController.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const Friend = require('./friendsModel');
+
+router
+.route('/')
+.get((req, res) = {
+
+})
+.post((req, res) => {
+
+})
+
+router
+.route('/:id')
+.get((req, res) => {
+
+})
+.delete((req, res) => {
+
+})
+.put((req, res) => {
+
+})
+
+module.exports = router;
