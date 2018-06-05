2381dc08d94fc1d56e894ce46bb7ffea84135b26
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-06 01:15:56 +0700

    added friendModel.js with schema for friend

---
+++ friends/friendModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const FriendSchema = new mongoose.Schema({
+
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 120
+  }
+});
+
+const bearsModel = mongoose.model('Friend', FriendSchema);
+
+module.exports = friendModel;
