4d447598aff7a603659a10b414f8c00105bf6526
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-06-05 23:22:50 -0500

    added scaffolding for react app

---
+++ client/src/components/FriendsList/FriendsList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './FriendsList.css';
+
+class FriendsList extends Component {
+    state = {
+        friends: []
+    }
+
+    componentDidMount() {
+
+    }
+
+
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+
+
+export default FriendsList;
