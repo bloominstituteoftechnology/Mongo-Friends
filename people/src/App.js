8356d96a6a87af89e5132051336353a8fe796411
unknown <sam.park.email@gmail.com>
2018-06-06 02:24:08 -0400

    fixed problem, now displaying db information in react app

--- people/src/App.js
+++ people/src/App.js
@@ -6,7 +6,7 @@ import Friends from './components/Friends'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ backgroundColor: "#c4d4ed" }} >
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
--- people/src/components/Friends.js
+++ people/src/components/Friends.js
@@ -1,36 +1,55 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Card } from 'reactstrap';
+import React, { Component } from "react";
+import axios from "axios";
+import { Card, Col, CardColumns } from "reactstrap";
 
 class Friends extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            friends: []
-         }
-    }
-    componentDidMount() {
-        axios
-        .get('http://localhost:5000/api/friends')
-        .then(response => {
-            console.log(response);
-            this.setState({ friends: response.data });
-        })
-        .catch(error => {
-            console.log("error:", error)
-        })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/friends")
+      .then(response => {
+        console.log(response);
+        this.setState({ friends: response.data });
+      })
+      .catch(error => {
+        console.log("error:", error);
+      });
+  }
 
-    render() { 
-        return ( 
-            <div>
-                {this.state.friends.map(friend => (
-                    console.log("friend:", friend)
-                    
-                ))}
-                </div>
-         )
-    }
+  render() {
+    return (
+      <div style={{ backgroundColor: "#c4d4ed" }}>
+        <CardColumns style={{ display: "flex" }}>
+          {this.state.friends.map(friend => (
+            <Col sm="3">
+              <Card
+                style={{
+                  paddingTop: "20px",
+                  paddingBottom: "20px",
+                  marginTop: "20px",
+                  border: "2px solid grey",
+                  boxShadow: "2px 2px 5px #6b6f75"
+                }}
+              >
+                {friend.firstName}
+                <br />
+                {friend.lastName}
+                <br />
+                {friend.contactInfo.phone}
+                <br />
+                {friend.contactInfo.email}
+              </Card>
+            </Col>
+          ))}
+        </CardColumns>
+      </div>
+    );
+  }
 }
- 
+
 export default Friends;
