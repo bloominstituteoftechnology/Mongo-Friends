46de5a9f9860727c3407548849fa85e0ee2da1e0
JSNavey <jsnavey@gmail.com>
2018-06-12 17:11:09 -0400

    finished API for route with id

--- friends/friendController.js
+++ friends/friendController.js
@@ -40,5 +40,61 @@ router
             })
         })
 
+router
+    .route('/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        Friend
+            .findById(id)
+            .then(foundFriend => {
+                if(!foundFriend) {
+                    errorMessage(404, 'No friend with this id found.', res);
+                }
+                res.status(200).json({ foundFriend })
+            })
+            .catch(error => {
+                errorMessage(500, 'Oops something wrong with our server.', res);
+            })
+    })
+
+    .delete((req, res) => {
+        const { id } = req.params;
+        Friend
+            .findByIdAndRemove(id)
+            .then(deletedFriend => {
+                if(!deletedFriend) {
+                    errorMessage(404, 'No friend with this id found.', res);
+                }
+                res.status({ deletedFriend })
+            })
+            .catch(error => {
+                errorMessage(500, 'Oops something wrong with our server.', res);
+            })
+    })
+
+    .put((req, res) => {
+        const { id } = req.params;
+        const { firstName, lastName, age } = req.body;
+
+        if(!firstName || !lastName || !age) {
+            errorMessage(400, 'Please provide your information.', res);
+        } 
+        if(age < 1 || age > 120) {
+            errorMessage(400, 'Please enter age between 1 to 120', res)
+        } 
+
+        Friend
+            .findByIdAndUpdate(id, {firstName, lastName, age })
+            .then(updatedFriend => {
+                if(!updatedFriend) {
+                    errorMessage(404, 'No friend with this id found.', res);
+                }
+                res.status(202).json({ updatedFriend })
+            })
+            .catch(error => {
+                errorMessage(500, 'Oops something wrong with our server.', res);
+            })
+    })
+
 
 module.exports = router;
