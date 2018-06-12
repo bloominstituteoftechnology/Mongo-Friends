741e0202b1a6587428c7dc2c88db13c23b0fba58
Bee C <b.choeung@gmail.com>
2018-06-12 19:07:06 -0400

    showed list of friends

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friends: [{}]
+    }
+  }
+  componentWillMount(){
+    axios.get('http://localhost:5000/api/friends')
+    .then (respond => {
+      console.log('data', respond.data)
+      this.setState ({ friends: respond.data})
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       {this.state.friends.map(friend => {
+         return (
+           <div>{friend.firstName}</div>
+         )
+       })}
       </div>
     );
   }
