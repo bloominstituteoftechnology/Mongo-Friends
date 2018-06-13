df22c47a1ac93f92bbba16318a051e7eea462173
JSNavey <jsnavey@gmail.com>
2018-06-12 23:16:09 -0400

    added update and delete button but yet functional
    need to do style too

--- client/src/components/Friend.js
+++ client/src/components/Friend.js
@@ -7,6 +7,8 @@ class Friend extends Component {
             <div key={this.props.friend.id}>
                 <h4><span>Name: {this.props.friend.lastName}, {this.props.friend.firstName}</span></h4>
                 <p>Age: {this.props.friend.age}</p>
+                <button>Update Friend</button>
+                <button>Bye Bye {this.props.friend.firstName}</button>
             </div>
         );
     }
